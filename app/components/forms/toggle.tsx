c7c52be8c2dcca18b30c376cdbee7083ee76b1f6
Daren Malfait <github@daren.be>
2022-02-18 13:46:21 +0100

    style(toggle): add icons that resemble state

--- app/components/forms/toggle.tsx
+++ app/components/forms/toggle.tsx
@@ -23,24 +23,58 @@ function Toggle({ disabled, id, onChange, value }: ToggleProps) {
         checked={enabled}
         onChange={setEnabled}
         className={clsx(
-          'inline-flex relative shrink-0 w-[50px] h-[30px] rounded-full border-2 border-transparent focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:opacity-75 transition-colors duration-200 ease-in-out cursor-pointer',
-          {
-            'bg-green-400': enabled,
-            'bg-gray-300': !enabled,
-          },
+          enabled ? 'bg-green-300' : 'bg-gray-200',
+          'inline-flex relative shrink-0 w-11 h-6 rounded-full border-2 border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 transition-colors duration-200 ease-in-out cursor-pointer focus:ring-primary',
         )}
       >
         <span className="sr-only">Use setting</span>
         <span
-          aria-hidden="true"
           className={clsx(
-            'inline-block w-[25px] h-[25px] bg-white rounded-full ring-0 shadow-lg transition duration-200 ease-in-out pointer-events-none',
-            {
-              'translate-x-5': enabled,
-              'translate-x-0': !enabled,
-            },
+            enabled ? 'translate-x-5' : 'translate-x-0',
+            'inline-block relative w-5 h-5 bg-white rounded-full ring-0 shadow transition duration-200 ease-in-out pointer-events-none',
           )}
-        />
+        >
+          <span
+            className={clsx(
+              enabled
+                ? 'opacity-0 duration-100 ease-out'
+                : 'opacity-100 duration-200 ease-in',
+              'flex absolute inset-0 justify-center items-center w-full h-full transition-opacity',
+            )}
+            aria-hidden="true"
+          >
+            <svg
+              className="w-3 h-3 text-gray-400"
+              fill="none"
+              viewBox="0 0 12 12"
+            >
+              <path
+                d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
+          <span
+            className={clsx(
+              enabled
+                ? 'opacity-100 duration-200 ease-in'
+                : 'opacity-0 duration-100 ease-out',
+              'flex absolute inset-0 justify-center items-center w-full h-full transition-opacity',
+            )}
+            aria-hidden="true"
+          >
+            <svg
+              className="w-3 h-3 text-indigo-600"
+              fill="currentColor"
+              viewBox="0 0 12 12"
+            >
+              <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+            </svg>
+          </span>
+        </span>
       </Switch>
     </button>
   )
