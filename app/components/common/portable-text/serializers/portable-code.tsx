803b9dcb5fc105304536e76b118f87628ba1ddaf
Daren Malfait <github@daren.be>
2022-02-22 23:38:14 +0100

    style: add custom prism theme

--- app/components/common/portable-text/serializers/portable-code.tsx
+++ app/components/common/portable-text/serializers/portable-code.tsx
@@ -1,8 +1,10 @@
 import { CodeBlock } from '@daren/ui-components'
-import theme from 'prism-react-renderer/themes/vsDark'
+
+import type { PrismTheme } from 'prism-react-renderer'
 import * as React from 'react'
 
 import type { Serialized } from './serializers.types'
+import theme from './vscode-theme'
 
 import { Container } from '~/components/layout'
 import type { SanityCode } from '~/types/sanity'
@@ -14,7 +16,7 @@ function PortableCode({ node }: Serialized<SanityCode>) {
     <Container size="small">
       <CodeBlock
         {...node}
-        theme={theme}
+        theme={theme as PrismTheme}
         className="!rounded-xl"
         language={node.language}
         showLanguage={lines > 1}
