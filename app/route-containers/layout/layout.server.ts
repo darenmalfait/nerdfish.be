4544f68d55730fbdb313e2e5db50d8e8b3b7cb20
Daren Malfait <github@daren.be>
2022-04-28 11:04:47 +0200

    fix: fallback when wrong default language

--- app/route-containers/layout/layout.server.ts
+++ app/route-containers/layout/layout.server.ts
@@ -1,4 +1,4 @@
-import { json } from 'remix'
+import { createCookie, json, redirect } from 'remix'
 import type { LoaderFunction } from 'remix'
 
 import type { Theme } from '~/context/theme-provider'
@@ -6,6 +6,7 @@ import { getSiteInfo } from '~/lib/api'
 import { i18n } from '~/lib/services/i18n.server'
 import { getThemeSession } from '~/lib/services/theme.server'
 import { ENV, getEnv } from '~/lib/utils/get-env'
+import { getDefaultLanguage } from '~/lib/utils/i18n'
 import { getDomainUrl } from '~/lib/utils/misc'
 import { pathedRoutes } from '~/other-routes.server'
 import type { Handle, SiteInfo } from '~/types'
@@ -40,6 +41,16 @@ export const loader: LoaderFunction = async ({ request }) => {
   ])
 
   const siteInfo = await getSiteInfo({ lang: locale })
+
+  if (!siteInfo.site?.multilang && locale !== getDefaultLanguage().code) {
+    return redirect('/', {
+      headers: {
+        'Set-Cookie': await createCookie('language').serialize(
+          getDefaultLanguage().code,
+        ),
+      },
+    })
+  }
 
   return json<LoaderData>({
     locale,
