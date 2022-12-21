4a30e7eb476129ffea4d06eb341e6a5226d87e25
Daren Malfait <me@daren.be>
2022-12-21 09:09:22 +0100

    fix(lib): add initial dark mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,46 @@
 import {Html, Head, Main, NextScript} from 'next/document'
 
+const modeScript = `
+  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+
+  updateMode()
+  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
+  window.addEventListener('storage', updateModeWithoutTransitions)
+
+  function updateMode() {
+    let isSystemDarkMode = darkModeMediaQuery.matches
+    let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
+
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+
+    if (isDarkMode === isSystemDarkMode) {
+      delete window.localStorage.isDarkMode
+    }
+  }
+
+  function disableTransitionsTemporarily() {
+    document.documentElement.classList.add('[&_*]:!transition-none')
+    window.setTimeout(() => {
+      document.documentElement.classList.remove('[&_*]:!transition-none')
+    }, 0)
+  }
+
+  function updateModeWithoutTransitions() {
+    disableTransitionsTemporarily()
+    updateMode()
+  }
+`
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script dangerouslySetInnerHTML={{__html: modeScript}} />
+      </Head>
       <body>
         <Main />
         <NextScript />
