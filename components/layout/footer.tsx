4292bc20c959c03900065c51de6f5a6ffb085a1a
Daren Malfait <github@daren.be>
2022-12-27 21:49:12 +0100

    style(footer): update styling (#40)

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,3 +1,5 @@
+import {ThemeToggle} from 'components/common/theme-toggle'
+
 import {useGlobal} from '../../context/global-provider'
 import {Link} from '../common/link'
 import {GithubIcon} from '../icons/github-icon'
@@ -5,60 +7,45 @@ import {Logo} from '../icons/logo'
 import {TwitterIcon} from '../icons/twitter-icon'
 
 function Footer() {
-  const {navigation, social} = useGlobal()
-
-  const items = [...(navigation?.main ?? []), ...(navigation?.actions ?? [])]
+  const {social} = useGlobal()
 
   const github = social?.github
   const twitter = social?.twitter
 
   return (
-    <footer className="mt-32 px-5vw">
-      <div className="mx-auto max-w-8xl border-t border-gray-300 py-24 dark:border-gray-700">
-        <div>
-          <Link
-            className="flex flex-initial items-center font-bold md:mr-24"
-            aria-label="home page"
-            href="/"
-          >
-            <Logo className="h-14 w-14" />
-          </Link>
+    <footer className="px-5vw pb-8 text-primary md:pb-20">
+      <div className="flex flex-col items-center justify-between gap-8 border-t border-gray-100 pt-8 dark:border-white/10 md:flex-row md:pt-16">
+        <div className="flex flex-col items-center gap-4 text-center md:flex-row md:text-left">
+          <div className="flex items-center">
+            <Logo className="relative h-12 w-12 rounded-full border-2 border-white dark:border-[#111]" />
+          </div>
+          <p className="max-w-md">
+            <b className="font-semibold">Made by Daren</b>, a freelance web
+            developer that keeps user experience in mind.
+          </p>
         </div>
-        <div className="grid grid-cols-1 gap-8 border-primary-100 py-12 transition-colors duration-150 bg-primary text-primary lg:grid-cols-12">
-          <div className="col-span-8">
-            <div>
-              {items.map(item => (
-                <Link
-                  key={item?.label}
-                  href={item?.href ?? '/'}
-                  isButton={item?.isButton}
-                  className="mr-8 inline-block text-lg"
-                  {...item}
-                >
-                  {item?.label}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className="col-span-2 flex items-start text-primary lg:col-span-4 lg:justify-end">
-            <div className="flex h-10 items-center space-x-4">
-              {twitter ? (
-                <a
-                  rel="noreferrer noopener"
-                  aria-label="Twitter feed"
-                  target="_blank"
-                  href={twitter}
-                >
-                  <TwitterIcon className="duration-75 ease-linear hover:scale-110" />
-                </a>
-              ) : null}
-              {github ? (
-                <Link aria-label="Github Repository" href={github}>
-                  <GithubIcon className="duration-75 ease-linear hover:scale-110" />
-                </Link>
-              ) : null}
-            </div>
-          </div>
+        <div className="flex flex-row items-center gap-3 md:gap-7">
+          <ThemeToggle className="flex h-10 w-10 items-center justify-center rounded-full transition hover:bg-github/20" />
+
+          {twitter ? (
+            <Link
+              className="flex h-10 w-10 items-center justify-center rounded-full transition focus-ring hover:bg-twitter/20"
+              aria-label="Twitter feed"
+              href={twitter}
+            >
+              <TwitterIcon className="duration-75 ease-linear" />
+            </Link>
+          ) : null}
+          {github ? (
+            <Link
+              className="flex h-10 w-10 items-center justify-center rounded-full transition focus-ring hover:bg-github/20"
+              aria-label="Github Repository"
+              href={github}
+            >
+              <GithubIcon className="duration-75 ease-linear" />
+              <span className="sr-only">Github</span>
+            </Link>
+          ) : null}
         </div>
       </div>
     </footer>
