5dc0e0151c0bd49fc1f00035389bed7236ba184b
Daren Malfait <me@daren.be>
2023-01-11 19:13:11 +0100

    style(code-block): adjust overall styling

--- components/common/code-block/code-block.tsx
+++ components/common/code-block/code-block.tsx
@@ -1,3 +1,4 @@
+import {ClipboardDocumentListIcon} from '@heroicons/react/24/solid'
 import clsx from 'clsx'
 import Highlight, {
   defaultProps,
@@ -5,8 +6,7 @@ import Highlight, {
   PrismTheme,
 } from 'prism-react-renderer'
 import nightOwl from 'prism-react-renderer/themes/nightOwl'
-
-import {ClipboardCopyButton} from './clipboard-copy-button'
+import * as React from 'react'
 
 function pad(num: number | string, size = 2) {
   num = num.toString()
@@ -16,6 +16,57 @@ function pad(num: number | string, size = 2) {
 }
 
 type CodeBlockLanguage = Language
+
+function CopyButton({code}: {code: string}) {
+  const [copyCount, setCopyCount] = React.useState(0)
+  const copied = copyCount > 0
+
+  React.useEffect(() => {
+    if (copyCount > 0) {
+      const timeout = setTimeout(() => setCopyCount(0), 1000)
+      return () => {
+        clearTimeout(timeout)
+      }
+    }
+  }, [copyCount])
+
+  return (
+    <button
+      type="button"
+      className={clsx(
+        'group/button absolute top-3.5 right-4 z-10 overflow-hidden rounded-full py-1 pl-2 pr-3 text-xs font-medium opacity-0 backdrop-blur transition text-primary focus:opacity-100 group-hover:opacity-100',
+        copied
+          ? 'bg-emerald-400/10 ring-1 ring-inset ring-emerald-400/20'
+          : 'hover:bg-white/7.5 dark:bg-white/2.5 bg-white/5 dark:hover:bg-white/5',
+      )}
+      onClick={async () => {
+        await window.navigator.clipboard.writeText(code).then(() => {
+          setCopyCount(count => count + 1)
+        })
+      }}
+    >
+      <span
+        aria-hidden={copied}
+        className={clsx(
+          'pointer-events-none flex items-center gap-0.5 text-gray-200 transition duration-300',
+          copied && '-translate-y-1.5 opacity-0',
+        )}
+      >
+        <ClipboardDocumentListIcon className="fill-gray-500/20 h-5 w-5 stroke-gray-500 transition-colors group-hover/button:stroke-gray-400" />
+        Copy
+      </span>
+      <span
+        aria-hidden={!copied}
+        className={clsx(
+          'pointer-events-none absolute inset-0 flex items-center justify-center text-emerald-400 transition duration-300',
+          !copied && 'translate-y-1.5 opacity-0',
+        )}
+      >
+        Copied!
+      </span>
+    </button>
+  )
+}
 
 function CodeBlock({
   code = ``,
@@ -42,15 +93,12 @@ function CodeBlock({
       >
         {({className, style, tokens, getLineProps, getTokenProps}) => (
           <>
-            <ClipboardCopyButton
-              value={code}
-              className="absolute top-3 right-3 z-10"
-            />
+            <CopyButton code={code} />
             <pre
               className={clsx(
                 classNameProp,
                 className,
-                'relative  my-5 mx-auto overflow-x-auto rounded-xl py-5 pr-8 text-sm leading-relaxed',
+                'relative my-5 mx-auto overflow-x-auto rounded-xl  !bg-black/90 py-5 pr-8 text-sm leading-relaxed dark:!bg-white/5',
                 {
                   'pl-4': !showLineNumbers,
                   'pl-16': showLineNumbers,
