41a3cb12ee7c4bda1f099e0d7b10360f4a56fc4c
Daren Malfait <me@daren.be>
2023-01-12 08:47:17 +0100

    style(code-block): add light theme

--- components/common/portable-text/portable-code.tsx
+++ components/common/portable-text/portable-code.tsx
@@ -1,17 +1,14 @@
 import {Container} from '@daren/ui-components'
-import type {Language, PrismTheme} from 'prism-react-renderer'
+import type {Language} from 'prism-react-renderer'
 import * as React from 'react'
 
 import {CodeBlock} from '../code-block/code-block'
-
-import theme from './vscode-theme'
 
 function PortableCode({lang, value}: {lang?: Language; value?: string}) {
   return (
     <Container size="default">
       <CodeBlock
         code={value}
-        theme={theme as PrismTheme}
         className="!rounded-xl"
         language={lang}
         showLanguage={false}
