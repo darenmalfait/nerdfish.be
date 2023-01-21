88f854acd5f27e736cd20d24a623479479a45cc9
Daren Malfait <me@daren.be>
2023-01-21 21:21:51 +0100

    style(global): change inter to cal font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ module.exports = {
     extend: {
       fontFamily: {
         fallback: [...defaultTheme.fontFamily.sans],
-        title: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
-        sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
+        title: ['var(--font-cal)', ...defaultTheme.fontFamily.sans],
+        sans: ['var(--font-cal)', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         twitter: '#1DA1F2',
