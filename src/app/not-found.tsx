6ec6e767511e885cc705249d2cc3f9383976b63c
Daren Malfait <daren@nerdfish.be>
2023-12-26 09:38:12 +0100

    feat(pages): add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import {ErrorPage} from './_components/errors'
+
+export default function Custom404() {
+  return (
+    <ErrorPage
+      title="404 - Page doesn't exist"
+      subTitle="Sorry, we couldn't find the page you were looking for."
+    />
+  )
+}
