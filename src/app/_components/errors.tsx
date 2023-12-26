c83248a697bbc8f62e5a5a1c3bc41357af845be0
Daren Malfait <daren@nerdfish.be>
2023-12-26 09:38:12 +0100

    feat(pages): add not-found page

--- src/app/_components/errors.tsx
+++ src/app/_components/errors.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image'
 import {Container, Grid, Paragraph, Section} from '@nerdfish/ui'
 import {Bookmark, ChevronRight, Rss} from 'lucide-react'
 
+import errorImage from '~/assets/images/fish-error.png'
 import {BigTitle} from '~/components/blocks/big-title'
 import {ArrowLink} from '~/components/common/arrow-link'
 import global from '~/content/global/index.json'
@@ -99,10 +101,11 @@ function ErrorPage({
                 </div>
               </div>
 
-              <div className="relative mx-auto flex w-full items-center sm:w-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:h-full lg:w-1/2">
-                <img
+              <div className="relative mx-auto flex w-full items-center overflow-hidden rounded-full sm:w-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:h-full lg:w-1/2">
+                <Image
+                  width={200}
                   className="max-w-96 mx-auto w-full rounded-md"
-                  src="/images/cat-on-box.png"
+                  src={errorImage}
                   alt="not found"
                 />
               </div>
@@ -114,24 +117,4 @@ function ErrorPage({
   )
 }
 
-function FourOhFour({title, subTitle}: {title?: string; subTitle?: string}) {
-  return (
-    <ErrorPage
-      title={title ?? "404 - Page doesn't exist"}
-      subTitle={
-        subTitle ?? "Sorry, we couldn't find the page you were looking for."
-      }
-    />
-  )
-}
-
-function ServerError({error}: {error?: Error}) {
-  return (
-    <ErrorPage
-      title="500 - Oh no, something went wrong!"
-      subTitle={error?.message}
-    />
-  )
-}
-
-export {FourOhFour, ServerError}
+export {ErrorPage}
---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import {ErrorPage} from './_components/errors'
+
+export default function Error({error}: {error?: Error}) {
+  return (
+    <ErrorPage
+      title="500 - Oh no, something went wrong!"
+      subTitle={error?.message}
+    />
+  )
+}
