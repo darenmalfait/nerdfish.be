59167d831b55803cd2be621106b4f6af6aa67feb
Daren Malfait <daren@nerdfish.be>
2023-12-29 10:19:02 +0100

    refactor(sitemap): use nextjs feature

--- next.config.mjs
+++ next.config.mjs
@@ -19,10 +19,6 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
-      },
-      {
         source: '/rss.xml',
         destination: '/api/feed/rss',
       },
