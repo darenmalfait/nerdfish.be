c6cc94f4f5788df090745932a54a360fa583bf1a
Daren Malfait <daren@nerdfish.be>
2024-02-02 08:35:42 +0100

    chore(deps): update eslint and prettier config

--- apps/web/src/lib/utils/form.server.ts
+++ apps/web/src/lib/utils/form.server.ts
@@ -1,79 +1,4 @@
-import {type NextApiResponse} from 'next'
-
 import {env} from '~/env.mjs'
-import {type NonNullProperties} from '~/lib/types/misc'
-
-import {getErrorMessage, getNonNull} from './misc'
-
-type ErrorMessage = string
-type NoError = null
-type FormValue = string | null
-
-async function handleFormSubmission<
-  ActionData extends {
-    status: 'success' | 'error' | string
-    fields: {[field: string]: FormValue}
-    errors: {[field: string]: ErrorMessage | NoError}
-  },
->({
-  values,
-  response,
-  validators,
-  // @ts-expect-error ts(2322) 🤷‍♂️
-  actionData = {fields: {}, errors: {}},
-  handleFormValues,
-}: {
-  validators: {
-    [Key in keyof ActionData['errors']]: (
-      formValue: FormValue,
-      fields: ActionData['fields'],
-    ) => Promise<ErrorMessage | NoError> | ErrorMessage | NoError
-  }
-  actionData?: ActionData
-  handleFormValues: (
-    formValues: NonNullProperties<ActionData['fields']>,
-  ) => Response | Promise<Response> | Promise<void>
-} & {
-  values: Record<string, string>
-  response?: NextApiResponse
-}): Promise<Response | Promise<void>> {
-  try {
-    // collect all values first because validators can reference them
-    for (const fieldName of Object.keys(validators)) {
-      const formValue = values[fieldName]
-      // Default the value to empty string so it doesn't have trouble with
-      // getNonNull later. This allows us to have a validator that allows
-      // for optional values.
-      actionData.fields[fieldName] = formValue ?? ''
-    }
-
-    await Promise.all(
-      Object.entries(validators).map(async ([fieldName, validator]) => {
-        const formValue = values[fieldName] ?? ''
-        // Default the value to empty string so it doesn't have trouble with
-        // getNonNull later. This allows us to have a validator that allows
-        // for optional values.
-        actionData.errors[fieldName] = await validator(
-          formValue,
-          actionData.fields,
-        )
-      }),
-    )
-
-    if (Object.values(actionData.errors).some(err => err !== null)) {
-      return response?.status(400).json({...actionData, status: 'error'})
-    }
-
-    const nonNullFields = getNonNull(actionData.fields)
-    // not sure why, but it wasn't happy without the type cast 🤷‍♂️
-    return await handleFormValues(
-      nonNullFields as NonNullProperties<ActionData['fields']>,
-    )
-  } catch (error: unknown) {
-    actionData.errors.generalError = getErrorMessage(error)
-    return response?.status(500).json(actionData)
-  }
-}
 
 async function getErrorForRecaptcha(
   recaptcha: string | null,
@@ -104,4 +29,4 @@ async function getErrorForRecaptcha(
   return null
 }
 
-export {getErrorForRecaptcha, handleFormSubmission}
+export {getErrorForRecaptcha}
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,6 +1,6 @@
 const {fontFamily} =
   // eslint-disable-next-line
-  require("tailwindcss/defaultTheme") as typeof import("tailwindcss/defaultTheme");
+  require('tailwindcss/defaultTheme') as typeof import('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
