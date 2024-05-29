54c60512256b847a6ceea783e2d0137f8e8bfa7d
Daren Malfait <daren@nerdfish.be>
2024-05-29 11:55:49 +0200

    style(navigation): update mobile navigation

--- apps/web/src/app/components/base-layout.tsx
+++ apps/web/src/app/components/base-layout.tsx
@@ -1,87 +1,26 @@
 'use client'
 
 import * as React from 'react'
-import {motion, MotionConfig, useReducedMotion} from 'framer-motion'
 
 import {Footer} from './footer'
 import {Header} from './header'
-import {NavigationGrid} from './navigation'
 
 export function BaseLayoutTemplate({children}: {children: React.ReactNode}) {
-  const panelId = React.useId()
-  const [expanded, setExpanded] = React.useState(false)
-  const openRef = React.useRef<React.ElementRef<'button'>>(null)
-  const closeRef = React.useRef<React.ElementRef<'button'>>(null)
-  const navRef = React.useRef<React.ElementRef<'div'>>(null)
-  const shouldReduceMotion = useReducedMotion()
-
   return (
-    <MotionConfig transition={shouldReduceMotion ? {duration: 0} : undefined}>
-      <div className="bg-inverted dark:bg-muted">
-        <header>
-          <div
-            className="absolute inset-x-0 top-2 z-40 pt-14"
-            aria-hidden={expanded ? 'true' : undefined}
-            // @ts-expect-error (https://github.com/facebook/react/issues/17157)
-            inert={expanded ? '' : undefined}
-          >
-            <Header
-              panelId={panelId}
-              toggleRef={openRef}
-              expanded={expanded}
-              onToggle={() => {
-                setExpanded(value => !value)
-                window.setTimeout(() =>
-                  closeRef.current?.focus({preventScroll: true}),
-                )
-              }}
-            />
-          </div>
+    <div className="bg-inverted dark:bg-muted">
+      <header>
+        <div className="absolute inset-x-0 top-2 z-40 pt-14">
+          <Header />
+        </div>
+      </header>
 
-          <motion.div
-            layout
-            id={panelId}
-            style={{height: expanded ? 'auto' : '0.5rem'}}
-            className="relative z-50 overflow-hidden bg-inverted pt-2 dark:bg-muted"
-            aria-hidden={expanded ? undefined : 'true'}
-            // @ts-expect-error (https://github.com/facebook/react/issues/17157)
-            inert={expanded ? undefined : ''}
-          >
-            <motion.div layout className="bg-inverted dark:bg-muted">
-              <div ref={navRef} className="pb-16 pt-14">
-                <Header
-                  invert
-                  panelId={panelId}
-                  toggleRef={closeRef}
-                  expanded={expanded}
-                  onToggle={() => {
-                    setExpanded(value => !value)
-                    window.setTimeout(() =>
-                      openRef.current?.focus({preventScroll: true}),
-                    )
-                  }}
-                />
-              </div>
-              <NavigationGrid />
-              <div className="pb-12" />
-            </motion.div>
-          </motion.div>
-        </header>
+      <div className="relative flex flex-auto overflow-hidden rounded-t-3xl bg-primary pt-14">
+        <div className="relative isolate flex w-full flex-col pt-24">
+          <main className="w-full flex-auto">{children}</main>
 
-        <motion.div
-          layout
-          className="relative flex flex-auto overflow-hidden rounded-t-3xl bg-primary pt-14"
-        >
-          <motion.div
-            layout
-            className="relative isolate flex w-full flex-col pt-24"
-          >
-            <main className="w-full flex-auto">{children}</main>
-
-            <Footer />
-          </motion.div>
-        </motion.div>
+          <Footer />
+        </div>
       </div>
-    </MotionConfig>
+    </div>
   )
 }
--- apps/web/src/app/components/header.tsx
+++ apps/web/src/app/components/header.tsx
@@ -5,31 +5,15 @@ import Link from 'next/link'
 import {usePathname} from 'next/navigation'
 import {stripPreSlash} from '@nerdfish-website/lib/utils'
 import {Icons} from '@nerdfish-website/ui/icons'
-import {Button} from '@nerdfish/ui'
 import {cx} from '@nerdfish/utils'
 
 import {useGlobal} from '../global-provider'
+import {ActionsNavigation, MobileNavigation, RSSFeedButton} from './navigation'
 import {ThemeToggle} from './theme-toggle'
 
-export function Header({
-  panelId,
-  expanded,
-  onToggle,
-  toggleRef,
-  invert,
-}: {
-  panelId: string
-  expanded: boolean
-  onToggle?: () => void
-  toggleRef: React.RefObject<HTMLButtonElement>
-  invert?: boolean
-}) {
+export function Header() {
   const {navigation} = useGlobal()
   const pathname = usePathname()
-
-  const buttonStyles = invert
-    ? 'text-inverted dark:text-primary hover:bg-primary/20 dark:hover:bg-primary'
-    : ''
 
   return (
     <div className="container mx-auto px-4">
@@ -39,78 +23,36 @@ export function Header({
             <Icons.Logo className="h-5 w-auto transition-transform hover:scale-105" />
           </Link>
         </div>
-        {!expanded ? (
-          <div className="hidden flex-1 justify-end md:justify-center lg:flex">
-            <nav className="flex rounded-full px-3 text-sm font-medium lg:space-x-2">
-              {navigation?.main?.map((link, i: number) => {
-                if (!link) return null
-                const isActive = stripPreSlash(pathname ?? '').startsWith(
-                  link.href,
-                )
-
-                return (
-                  <Link
-                    key={i}
-                    className={cx(
-                      'flex h-8 items-center cursor-pointer gap-x-1.5 active-ring !ring-muted rounded-full px-3 transition sm:h-10 sm:px-4 active:bg-inverted/10',
-                      isActive
-                        ? 'flex h-8 items-center gap-x-1.5 rounded-full bg-primary px-3 text-primary transition shadow-outline hover:text-primary/90 hover:bg-muted sm:h-10 sm:px-4'
-                        : 'hover:bg-muted',
-                    )}
-                    href={`/${stripPreSlash(link.href)}`}
-                  >
-                    {link.label}
-                  </Link>
-                )
-              })}
-            </nav>
-          </div>
-        ) : null}
-        <div className="flex justify-end space-x-2 md:flex-1">
-          <ThemeToggle variant="ghost" className={buttonStyles} />
-          <Button
-            variant="ghost"
-            size="icon"
-            className={cx(buttonStyles, 'hidden xsm:flex')}
-            asChild
-          >
-            <a
-              target="_blank"
-              href="/rss.xml"
-              aria-label="rss feed"
-              rel="noreferrer"
-            >
-              <span className="sr-only">RSS feed</span>
-              <Icons.Rss className="size-4" />
-            </a>
-          </Button>
-
-          <div className="hidden space-x-3 sm:flex">
-            {navigation?.actions?.map((link, i: number) => {
+        <div className="hidden flex-1 justify-end md:justify-center lg:flex">
+          <nav className="flex rounded-full px-3 text-sm font-medium lg:space-x-2">
+            {navigation?.main?.map((link, i: number) => {
               if (!link) return null
+              const isActive = stripPreSlash(pathname ?? '').startsWith(
+                link.href,
+              )
 
               return (
-                <Button key={i} asChild>
-                  <Link href={`/${stripPreSlash(link.href)}`}>
-                    {link.label}
-                  </Link>
-                </Button>
+                <Link
+                  key={i}
+                  className={cx(
+                    'flex h-8 items-center cursor-pointer gap-x-1.5 active-ring !ring-muted rounded-full px-3 transition sm:h-10 sm:px-4 active:bg-inverted/10',
+                    isActive
+                      ? 'flex h-8 items-center gap-x-1.5 rounded-full bg-primary px-3 text-primary transition shadow-outline hover:text-primary/90 hover:bg-muted sm:h-10 sm:px-4'
+                      : 'hover:bg-muted',
+                  )}
+                  href={`/${stripPreSlash(link.href)}`}
+                >
+                  {link.label}
+                </Link>
               )
             })}
-          </div>
-          <Button
-            variant="ghost"
-            ref={toggleRef}
-            type="button"
-            size="icon"
-            onClick={onToggle}
-            aria-expanded={expanded ? 'true' : 'false'}
-            aria-controls={panelId}
-            className={cx('group', buttonStyles)}
-            aria-label="Toggle navigation"
-          >
-            <Icons.Hamburger className="size-4" />
-          </Button>
+          </nav>
+        </div>
+        <div className="flex justify-end space-x-2 md:flex-1">
+          <ThemeToggle variant="ghost" />
+          <RSSFeedButton className="hidden xsm:flex" />
+          <ActionsNavigation className="hidden md:flex" />
+          <MobileNavigation />
         </div>
       </div>
     </div>
