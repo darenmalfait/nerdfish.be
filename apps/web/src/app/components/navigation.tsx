e5c0fe532f32b662113dd763cc9f2d1583745024
Daren Malfait <daren@nerdfish.be>
2024-05-29 11:55:49 +0200

    style(navigation): update mobile navigation

--- apps/web/src/app/components/navigation.tsx
+++ apps/web/src/app/components/navigation.tsx
@@ -3,62 +3,114 @@
 import * as React from 'react'
 import Link from 'next/link'
 import {stripPreSlash} from '@nerdfish-website/lib/utils'
+import {Icons} from '@nerdfish-website/ui/icons'
+import {Button, Drawer, H2} from '@nerdfish/ui'
+import {cx} from '@nerdfish/utils'
 
 import {useGlobal} from '../global-provider'
+import {ThemeToggle} from './theme-toggle'
 
-function NavigationRow({children}: {children: React.ReactNode}) {
+const NavigationItem = React.forwardRef(
+  (
+    {href, children, ...props}: React.ComponentPropsWithRef<'a'>,
+    ref: React.Ref<HTMLAnchorElement>,
+  ) => {
+    return (
+      <Link href={`/${stripPreSlash(href ?? '')}`} ref={ref} {...props}>
+        <H2
+          className="hover:text-muted"
+          blurredClassName="hidden"
+          variant="primary"
+          as="span"
+        >
+          {children}
+        </H2>
+      </Link>
+    )
+  },
+)
+NavigationItem.displayName = 'NavigationItem'
+
+export function RSSFeedButton({className}: {className?: string}) {
   return (
-    <div className="container mx-auto px-2 even:mt-px dark:bg-muted sm:bg-inverted">
-      <div className="grid grid-cols-1 sm:grid-cols-2">{children}</div>
+    <Button variant="ghost" size="icon" className={className} asChild>
+      <a target="_blank" href="/rss.xml" aria-label="rss feed" rel="noreferrer">
+        <span className="sr-only">RSS feed</span>
+        <Icons.Rss className="size-4" />
+      </a>
+    </Button>
+  )
+}
+
+export function ActionsNavigation({
+  className,
+  onSelect,
+}: {
+  className?: string
+  onSelect?: () => void
+}) {
+  const {navigation} = useGlobal()
+
+  return (
+    <div className={cx('space-x-3', className)}>
+      {navigation?.actions?.map((link, i: number) => {
+        if (!link) return null
+
+        return (
+          <Button key={i} onClick={onSelect} asChild>
+            <Link href={`/${stripPreSlash(link.href)}`}>{link.label}</Link>
+          </Button>
+        )
+      })}
     </div>
   )
 }
 
-function NavigationItem({
-  href,
-  children,
-}: {
-  href: string
-  children: React.ReactNode
-}) {
-  return (
-    <Link
-      href={`/${stripPreSlash(href)}`}
-      className="group relative isolate rounded-md bg-inverted !px-6 py-10 text-inverted even:mt-px hover:bg-white/10 dark:bg-muted dark:text-primary dark:hover:bg-black/10 md:mx-0 md:px-0 md:py-16 md:odd:pr-16 md:even:mt-0 md:even:border-l md:even:border-primary/20"
-    >
-      {children}
-    </Link>
-  )
-}
-
-export function NavigationGrid() {
+export function MobileNavigation() {
   const {navigation} = useGlobal()
+  const [open, setOpen] = React.useState<boolean>(false)
 
-  const navigationGroups = React.useMemo(() => {
-    const items = navigation?.main ?? []
-
-    // group items per 2 in a new array
-    const rows: Array<Array<(typeof items)[number]>> = []
-    for (let i = 0; i < items.length; i += 2) {
-      rows.push(items.slice(i, i + 2))
-    }
-
-    return rows
-  }, [navigation?.main])
   return (
-    <nav className="font-display mt-px text-5xl font-medium tracking-tight text-inverted">
-      {navigationGroups.map((row, i) => (
-        <NavigationRow key={i}>
-          {row.map((link, j) => {
-            if (!link) return null
-            return (
-              <NavigationItem key={j} href={link.href}>
-                {link.label}
-              </NavigationItem>
-            )
-          })}
-        </NavigationRow>
-      ))}
-    </nav>
+    <Drawer direction="top" open={open} onOpenChange={setOpen}>
+      <Drawer.Trigger asChild>
+        <Button
+          className="lg:hidden"
+          variant="ghost"
+          type="button"
+          size="icon"
+          aria-label="Toggle navigation"
+        >
+          <Icons.Hamburger className="size-4" />
+        </Button>
+      </Drawer.Trigger>
+      <Drawer.Content>
+        <div className="flex flex-col gap-12 py-6">
+          <div className="container m-auto flex flex-col items-center justify-center gap-6">
+            {navigation?.main?.map((link, i: number) => {
+              if (!link) return null
+
+              return (
+                <NavigationItem
+                  key={i}
+                  href={link.href}
+                  onClick={() => setOpen(false)}
+                >
+                  {link.label}
+                </NavigationItem>
+              )
+            })}
+          </div>
+          <div className="flex justify-center space-x-2 md:flex-1">
+            <ThemeToggle variant="ghost" />
+            <RSSFeedButton />
+            <ActionsNavigation
+              onSelect={() => {
+                setOpen(false)
+              }}
+            />
+          </div>
+        </div>
+      </Drawer.Content>
+    </Drawer>
   )
 }
