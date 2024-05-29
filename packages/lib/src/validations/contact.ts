02cbd52d2804663b81b1aa61af95e425a124a918
Daren Malfait <daren@nerdfish.be>
2024-05-29 13:04:09 +0200

    feat(form): replace headlessui radiogroup

--- packages/lib/src/validations/contact.ts
+++ packages/lib/src/validations/contact.ts
@@ -1,10 +1,22 @@
 import * as z from 'zod'
 
 export const contactSchema = z.object({
-  name: z.string().min(3).max(32),
-  email: z.string().email(),
-  project: z.string().min(3).max(32),
-  textMessage: z.string().min(3).max(512),
+  name: z
+    .string()
+    .min(2, 'Your name seems a bit short.')
+    .max(32, 'Your name seems a bit long.'),
+  email: z.string().email('Please enter a valid email address.'),
+  project: z
+    .string()
+    .min(3, 'Does not seem to be a valid project')
+    .max(32, 'Does not seem to be a valid project'),
+  textMessage: z
+    .string()
+    .min(10, 'I hope that your message is a bit longer than that.')
+    .max(
+      512,
+      'I like that you have a lot to say, but please keep it under 512 characters.',
+    ),
   recaptchaResponse: z.string().optional(),
 })
 
