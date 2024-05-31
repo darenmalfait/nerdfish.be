73f9723409327aee957c26620a41581eed47466c
Daren Malfait <56068461+darenmalfait@users.noreply.github.com>
2024-05-31 12:26:53 +0200

    feat(global): revamp main navigation (#291)

---
+++ apps/web/src/app/components/mobile-navigation.tsx
@@ -0,0 +1,156 @@
+'use client'
+
+import * as React from 'react'
+import Link from 'next/link'
+import {usePathname} from 'next/navigation'
+import {stripPreSlash} from '@nerdfish-website/lib/utils'
+import {Icons} from '@nerdfish-website/ui/icons'
+import {Button, Drawer, H2, H3} from '@nerdfish/ui'
+import {cx} from '@nerdfish/utils'
+
+import {type GlobalNavigationMain, type GlobalNavigationMainSub} from '../cms'
+import {useGlobal} from '../global-provider'
+import {ActionsNavigation, RSSFeedButton, SocialLinks} from './navigation'
+import {ThemeToggle} from './theme-toggle'
+
+const MobileNavigationSubItem = React.forwardRef<
+  React.ElementRef<typeof Link>,
+  React.ComponentPropsWithoutRef<typeof Link> & GlobalNavigationMainSub
+>(({href, label, description, className, ...props}, ref) => {
+  const pathname = usePathname()
+  const isActive = stripPreSlash(pathname ?? '').startsWith(href)
+
+  return (
+    <li>
+      <Link ref={ref} href={`/${stripPreSlash(href)}`} {...props}>
+        <H3
+          className={cx(
+            'hover:text-muted capitalize',
+            isActive && 'text-muted',
+          )}
+          as="span"
+        >
+          {label}
+        </H3>
+      </Link>
+    </li>
+  )
+})
+MobileNavigationSubItem.displayName = 'MobileNavigationSubItem'
+
+function MobileNavigationItem({
+  href,
+  label,
+  sub,
+  onClick,
+}: GlobalNavigationMain & {
+  onClick: () => void
+}) {
+  const pathname = usePathname()
+  if (!sub?.length && !href) return null
+
+  if (!sub?.length) {
+    const isActive = stripPreSlash(pathname ?? '').startsWith(href ?? '')
+
+    return (
+      <Link href={`/${stripPreSlash(href ?? '')}`} onClick={onClick}>
+        <H2
+          className={cx(
+            'hover:text-muted capitalize',
+            isActive && 'text-muted',
+          )}
+          blurredClassName="hidden"
+          variant="primary"
+          as="span"
+        >
+          {label}
+        </H2>
+      </Link>
+    )
+  }
+
+  return (
+    <div>
+      <H2
+        className="capitalize"
+        blurredClassName="hidden"
+        variant="primary"
+        as="span"
+      >
+        {label}
+      </H2>
+      <ul className="mt-3 grid gap-3">
+        {sub.map(subNavItem => {
+          if (!subNavItem) return null
+
+          return (
+            <MobileNavigationSubItem
+              key={subNavItem.label}
+              {...subNavItem}
+              onClick={onClick}
+            />
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export function MobileNavigation() {
+  const {navigation} = useGlobal()
+  const [open, setOpen] = React.useState<boolean>(false)
+
+  return (
+    <Drawer direction="top" open={open} onOpenChange={setOpen}>
+      <Drawer.Trigger asChild>
+        <Button
+          className="lg:hidden"
+          variant="ghost"
+          type="button"
+          size="icon"
+          aria-label="Toggle navigation"
+        >
+          <Icons.Hamburger className="size-4" />
+        </Button>
+      </Drawer.Trigger>
+      <Drawer.Content className="dark:bg-popover">
+        <div className="container mx-auto flex flex-col gap-12 px-4 py-6">
+          <div className="flex items-center justify-between gap-2">
+            <Link href="/" aria-label="Home" onClick={() => setOpen(false)}>
+              <Icons.Logo className="h-5 w-auto transition-transform hover:scale-105" />
+            </Link>
+            <div className="flex flex-row items-center justify-end gap-2">
+              <ThemeToggle variant="ghost" />
+              <RSSFeedButton />
+              <ActionsNavigation
+                onSelect={() => {
+                  setOpen(false)
+                }}
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+            {navigation?.main?.map(navItem => {
+              if (!navItem) return null
+
+              return (
+                <MobileNavigationItem
+                  key={navItem.label}
+                  {...navItem}
+                  onClick={() => setOpen(false)}
+                />
+              )
+            })}
+          </div>
+
+          <div className="flex gap-2">
+            <div className="flex flex-row items-center gap-3">
+              <span className="font-bold">Socials:</span>
+              <SocialLinks />
+            </div>
+          </div>
+        </div>
+      </Drawer.Content>
+    </Drawer>
+  )
+}
--- packages/ui/src/icons.tsx
+++ packages/ui/src/icons.tsx
@@ -99,6 +99,20 @@ export const Icons = {
   Edit,
   Ellipsis: MoreVertical,
   ExternalLink,
+  Facebook: ({...props}: LucideProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+    </svg>
+  ),
   Feature: Lightbulb,
   Filter,
   Flag,
@@ -111,10 +125,40 @@ export const Icons = {
   Help: HelpCircle,
   Home,
   Info,
+  Instagram: ({...props}: LucideProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+      <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37zM17.5 6.5h.01" />
+    </svg>
+  ),
   Issue: CircleDot,
   Italic,
   Key,
   Link: Link2,
+  LinkedIn: ({...props}: LucideProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6zM2 9h4v12H2z" />
+      <circle cx="4" cy="4" r="2" />
+    </svg>
+  ),
   List,
   Lock,
   Logo: ({...props}: LucideProps) => (
