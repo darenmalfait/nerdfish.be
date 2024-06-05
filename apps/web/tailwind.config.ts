ebbdc637ae9cf8245df0d036ddfa8c2fcf0b1e74
Daren Malfait <daren@nerdfish.be>
2024-06-05 13:30:04 +0200

    fix(web): add missing package

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -44,6 +44,7 @@ module.exports = {
     },
   },
   plugins: [
+    require('tailwindcss-animate'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@nerdfish/tailwind-config'),
