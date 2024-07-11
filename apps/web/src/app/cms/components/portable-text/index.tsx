19db2c6eb0a59f1c559e9b155e65cfe58cc37839
Daren Malfait <56068461+darenmalfait@users.noreply.github.com>
2024-07-11 08:40:40 +0200

    feat(portable-heading): add linkable headings (#315)

--- apps/web/src/app/cms/components/portable-text/index.tsx
+++ apps/web/src/app/cms/components/portable-text/index.tsx
@@ -6,6 +6,7 @@ import { TinaMarkdown } from 'tinacms/dist/rich-text'
 import { PortableButton } from './portable-button'
 import { PortableCode } from './portable-code'
 import { PortableContactForm } from './portable-contact-form'
+import { PortableHeading } from './portable-heading'
 import { PortableImage } from './portable-image'
 import { PortableLink } from './portable-link'
 
@@ -19,6 +20,10 @@ function PortableText(props: ExtractProps<typeof TinaMarkdown>) {
 					ContactForm: PortableContactForm,
 					code_block: PortableCode,
 					image: PortableImage,
+					h1: PortableHeading,
+					h2: PortableHeading,
+					h3: PortableHeading,
+					h4: PortableHeading,
 					a: PortableLink,
 				} as any
 			}
