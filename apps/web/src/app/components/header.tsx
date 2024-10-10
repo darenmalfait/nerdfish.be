a9e497554676d7079a850190891b7d7e93a274ac
Daren Malfait <daren@nerdfish.be>
2024-10-10 16:06:20 +0200

    style(navigation): adjust navigation

--- apps/web/src/app/components/header.tsx
+++ apps/web/src/app/components/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button } from '@nerdfish/ui'
+import { Button, Separator } from '@nerdfish/ui'
 import { Icons } from '@nerdfish-website/ui/icons'
 import Link from 'next/link'
 
@@ -10,27 +10,33 @@ import { ThemeToggle } from './theme-toggle'
 
 export function Header() {
 	return (
-		<div className="absolute top-8 z-50 mx-auto w-full px-4">
-			<div className="bg-muted/80 relative mx-auto flex w-full items-center justify-center gap-4 rounded-full px-3 py-2 sm:w-fit sm:gap-6 xl:min-w-[65ch]">
-				<div className="flex-1 sm:flex-grow-0">
-					<Button asChild variant="ghost">
-						<Link href="/" aria-label="Home">
-							<Icons.Logo className="h-5 w-auto" />
-						</Link>
-					</Button>
-				</div>
-				<div className="hidden justify-end sm:flex md:justify-center">
-					<nav className="flex rounded-full px-3 text-sm font-medium sm:space-x-2">
-						<MainNavigation />
-					</nav>
-				</div>
-				<div className="flex flex-1 justify-end space-x-2 sm:flex-grow-0">
-					<ThemeToggle variant="ghost" />
-					<RSSFeedButton className="xsm:flex hidden" />
-					<ActionsNavigation className="hidden md:flex" />
-					<MobileNavigation />
-				</div>
-			</div>
+		<div className="w-full">
+			<header className="relative z-50 w-full flex-none">
+				<nav className="mx-auto w-full px-4 lg:container">
+					<div className="relative flex w-full items-center py-[2.125rem]">
+						<Button asChild variant="link" className="-mx-2">
+							<Link href="/" aria-label="Home">
+								<Icons.Logo className="h-5 w-auto" />
+							</Link>
+						</Button>
+						<div className="ml-auto flex items-center">
+							<div className="hidden flex-1 lg:flex">
+								<MainNavigation />
+							</div>
+							<Separator
+								orientation="vertical"
+								className="mx-2 hidden h-6 lg:block"
+							/>
+							<div className="flex flex-1 justify-end space-x-2 sm:flex-grow-0">
+								<ThemeToggle variant="ghost" />
+								<RSSFeedButton className="xsm:flex hidden" />
+								<ActionsNavigation className="hidden md:flex" />
+								<MobileNavigation />
+							</div>
+						</div>
+					</div>
+				</nav>
+			</header>
 		</div>
 	)
 }
