2f3dcc0f5d975404b0c93dad39140d06b29b48d2
Daren Malfait <daren@nerdfish.be>
2024-10-11 14:20:17 +0200

    feat(sections): add new skills section

--- apps/web/tina/schema/blocks.ts
+++ apps/web/tina/schema/blocks.ts
@@ -5,6 +5,7 @@ import { heroTemplate } from './blocks/hero.template'
 import { highlightsTemplate } from './blocks/highlights.template'
 import { keywordListTemplate } from './blocks/keyword-list.template'
 import { productsTemplate } from './blocks/products.template'
+import { skillsTemplate } from './blocks/skills.template'
 import { wikiTemplate } from './blocks/wiki.template'
 import { workTemplate } from './blocks/work.template'
 
@@ -16,6 +17,7 @@ const blocks = [
 	highlightsTemplate,
 	keywordListTemplate,
 	productsTemplate,
+	skillsTemplate,
 	wikiTemplate,
 	workTemplate,
 ]
---
+++ apps/web/tina/schema/blocks/skills.template.ts
@@ -0,0 +1,45 @@
+import { type Template } from 'tinacms'
+
+export const skills = [
+	'javascript',
+	'typescript',
+	'node',
+	'react',
+	'sass',
+	'vscode',
+	'css',
+	'html',
+	'git',
+	'figma',
+	'next',
+] as const
+
+export const skillsTemplate: Template = {
+	name: 'Skills',
+	label: 'Skills',
+	ui: {
+		previewSrc: '/blocks/keyword-list.png',
+		defaultItem: {
+			items: [],
+		},
+	},
+	fields: [
+		{
+			type: 'string',
+			label: 'Title',
+			name: 'title',
+		},
+		{
+			type: 'rich-text',
+			label: 'Description',
+			name: 'description',
+		},
+		{
+			type: 'string',
+			label: 'Skills',
+			name: 'skills',
+			options: skills as any,
+			list: true,
+		},
+	],
+}
