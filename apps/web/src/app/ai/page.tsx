d74772d1942fff6e0456b4f5c1c33c2a2e961508
Daren Malfait <daren@nerdfish.be>
2024-10-12 08:27:18 +0200

    feat(ai-page): add metadata

--- apps/web/src/app/ai/page.tsx
+++ apps/web/src/app/ai/page.tsx
@@ -1,15 +1,30 @@
 import { Paragraph } from '@nerdfish/ui'
 import { Section } from '@nerdfish-website/ui/components'
-import { Header } from '../common'
+import { type Metadata } from 'next'
+import { generateOGImageUrl, getMetaData, Header } from '../common'
 import { Chat } from './components/chat'
+
+export async function generateMetadata(): Promise<Metadata | undefined> {
+	const title = 'Nerdfish - Daren Malfait AI'
+
+	return getMetaData({
+		ogImage: generateOGImageUrl({
+			heading: title,
+		}),
+		title,
+		url: '/ai',
+		description: 'Ask Daren Malfait anything',
+		canonical: '/ai',
+	})
+}
 
 export default function AiPage() {
 	return (
 		<Section>
 			<Header className="mb-3" title="AI" subtitle="Beta" />
 			<Paragraph className="mb-6 font-medium">
-				Disclaimer: This is not actually me, but AI. Answers might be
-				inaccurate.
+				Disclaimer: This is not actually me, but AI. Answers might be highly
+				inaccurate, it is a fun experiment.
 			</Paragraph>
 			<Chat />
 		</Section>
