7440c80d315e77c87cadd40530cb30fa29b61951
Daren Malfait <daren@nerdfish.be>
2024-10-15 16:23:05 +0200

    fix(portable-text): fields cannot be empty

--- apps/web/src/app/cms/components/portable-text/portable-contact-form.tsx
+++ apps/web/src/app/cms/components/portable-text/portable-contact-form.tsx
@@ -1,11 +1,18 @@
 'use client'
 
+import { type ExtractProps } from '@nerdfish/utils'
 import * as React from 'react'
+import { TinaMarkdown } from 'tinacms/dist/rich-text'
 import { ContactForm } from '~/app/email'
 
-export function PortableContactForm() {
+export function PortableContactForm({
+	content,
+}: {
+	content?: ExtractProps<typeof TinaMarkdown>['content']
+}) {
 	return (
 		<div className="not-prose space-y-6">
+			{content ? <TinaMarkdown content={content} /> : null}
 			<ContactForm />
 		</div>
 	)
