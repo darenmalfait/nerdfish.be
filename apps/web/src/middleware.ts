673961d118262ef3196a7bd8f3d468745afff75d
Daren Malfait <daren@nerdfish.be>
2024-10-24 16:59:14 +0200

    fix(middleware): don't target sitemap and robot.txt

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -48,6 +48,6 @@ export function middleware(request: NextRequest) {
 export const config = {
 	// Matcher ignoring `/_next/` and `/api/`
 	matcher: [
-		'/((?!api|_next/static|_next/image|uploads|images|admin|favicon.ico).*)',
+		'/((?!api|_next/static|_next/image|uploads|images|admin|favicon.ico|sitemap.xml|robots.txt).*)',
 	],
 }
