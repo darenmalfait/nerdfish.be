0bb007b7cd3f004013d8199a9876bf9304151d32
Daren Malfait <daren@nerdfish.be>
2024-11-02 14:00:30 +0100

    refactor(contact-form): move send mail logic

--- apps/web/src/app/[locale]/contact/components/contact-form/actions.ts
+++ apps/web/src/app/[locale]/contact/components/contact-form/actions.ts
@@ -1,11 +1,41 @@
 'use server'
 
 import {
+	ContactEmail as contactEmail,
+	renderAsync,
+} from '@nerdfish-website/emails/emails'
+import {
 	contactSchema,
 	type ContactFormData,
 } from '@nerdfish-website/lib/validations'
+import type * as React from 'react'
+import { Resend } from 'resend'
+const resend = new Resend(process.env.RESEND_API_KEY)
 
-import { sendContactEmail } from '~/app/email'
+export async function sendContactEmail({
+	message,
+	from,
+}: {
+	message: string
+	from: string
+}) {
+	if (process.env.SKIP_EMAILS ?? !process.env.NERDFISH_SMTP) return
+
+	// https://github.com/resendlabs/resend-node/issues/256
+	const html = await renderAsync(
+		contactEmail({ message, from }) as React.ReactElement,
+	)
+
+	const data = await resend.emails.send({
+		from: process.env.NERDFISH_SMTP,
+		reply_to: from,
+		to: [process.env.NERDFISH_SMTP],
+		subject: 'Email from contact form nerdfish.be',
+		html,
+	})
+
+	return data
+}
 
 export async function submitContactForm(payload: ContactFormData) {
 	const data = contactSchema.parse(payload)
