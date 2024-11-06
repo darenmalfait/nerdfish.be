ad6cfe9d99db970bbb8956a8a08cf571ea738e19
Daren Malfait <daren@nerdfish.be>
2024-11-06 09:33:31 +0100

    fix(blog): remove clutter

--- apps/web/src/app/[locale]/blog/components/blog-content.tsx
+++ apps/web/src/app/[locale]/blog/components/blog-content.tsx
@@ -1,47 +1,17 @@
 import { H1 } from '@nerdfish/ui'
 import { cx } from '@nerdfish/utils'
-import {
-	ArticleCard,
-	ArticleCardCategory,
-	ArticleCardContent,
-	ArticleCardDescription,
-	ArticleCardImage,
-	ArticleCardTitle,
-	DateFormatter,
-	Section,
-} from '@nerdfish-website/ui/components'
+import { DateFormatter, Section } from '@nerdfish-website/ui/components'
 import Image from 'next/image'
 import * as React from 'react'
 import { tinaField } from 'tinacms/dist/react'
-import { mapBlogData } from '../api'
-import { getBlogPath } from '../utils'
 import { BackToBlog } from './misc'
 import { PortableText, type BlogPostQueryQuery } from '~/app/cms'
-import {
-	SectionHeader,
-	ReadingProgress,
-	SectionHeaderTitle,
-	SectionHeaderSubtitle,
-} from '~/app/common'
+import { ReadingProgress } from '~/app/common'
 
 const prose = 'prose dark:prose-invert md:prose-lg lg:prose-xl max-w-4xl'
 
 function BlogContent({ data }: { data: BlogPostQueryQuery }) {
-	const { title, date, tags, heroImg, body } = data.blog
-
-	const blockData = { ...mapBlogData(data) }
-	const { blogs: allPosts } = blockData
-
-	const relatedPosts = React.useMemo(() => {
-		return allPosts
-			.filter(
-				(post) =>
-					post.title !== title &&
-					post.date !== date &&
-					post.tags?.some((tag) => tags?.includes(tag)),
-			)
-			.slice(0, 3)
-	}, [allPosts, date, tags, title])
+	const { title, date, heroImg, body } = data.blog
 
 	return (
 		<article>
@@ -94,43 +64,6 @@ function BlogContent({ data }: { data: BlogPostQueryQuery }) {
 					</div>
 				) : null}
 			</Section>
-
-			{relatedPosts.length > 0 ? (
-				<Section>
-					<SectionHeader>
-						<SectionHeaderTitle animatedText="Done reading?" />
-						<SectionHeaderSubtitle>
-							Read more related articles
-						</SectionHeaderSubtitle>
-					</SectionHeader>
-					<ul className="gap-x-lg gap-y-xl relative grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12">
-						{relatedPosts.map((relatedBlog) => {
-							return (
-								<li key={relatedBlog.id} className="col-span-4">
-									<ArticleCard
-										href={getBlogPath(relatedBlog)}
-										title={relatedBlog.title}
-									>
-										<ArticleCardImage
-											src={relatedBlog.heroImg}
-											category={relatedBlog.category}
-										/>
-										<ArticleCardContent>
-											<ArticleCardCategory>
-												{relatedBlog.category}
-											</ArticleCardCategory>
-											<ArticleCardTitle>{relatedBlog.title}</ArticleCardTitle>
-											<ArticleCardDescription>
-												{relatedBlog.seo?.description}
-											</ArticleCardDescription>
-										</ArticleCardContent>
-									</ArticleCard>
-								</li>
-							)
-						})}
-					</ul>
-				</Section>
-			) : null}
 		</article>
 	)
 }
