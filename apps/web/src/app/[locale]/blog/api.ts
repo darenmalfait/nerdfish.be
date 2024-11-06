f073dc9c0df622f54163d226f4a3bdd412bc9368
Daren Malfait <daren@nerdfish.be>
2024-11-06 09:33:31 +0100

    fix(blog): remove clutter

--- apps/web/src/app/[locale]/blog/api.ts
+++ apps/web/src/app/[locale]/blog/api.ts
@@ -1,4 +1,4 @@
-import { tina, type Blog, type BlogPostQueryQuery } from '../../cms'
+import { tina, type BlogPostQueryQuery } from '../../cms'
 import { type Locale } from '~/app/i18n'
 
 export async function getBlogPosts({ locale }: { locale?: Locale } = {}) {
@@ -17,23 +17,13 @@ export async function getBlogPosts({ locale }: { locale?: Locale } = {}) {
 export function mapBlogData(data: BlogPostQueryQuery) {
 	return {
 		...data,
-		blogs: data.blogConnection.edges?.map((item: any) => ({
-			...(item?.node ?? {}),
-		})) as Blog[],
 	}
 }
 
 export async function getBlogPost(relativePath: string) {
-	const blog = await tina.queries
+	return tina.queries
 		.blogPostQuery({
 			relativePath,
 		})
 		.catch(() => null)
-
-	if (!blog) return null
-
-	return {
-		...blog,
-		data: mapBlogData(blog.data),
-	}
 }
