5839e3f42bef07d4ddfcc34f8919f7bee4eba0f2
Daren Malfait <daren@nerdfish.be>
2024-11-08 10:32:07 +0100

    feat(keyword-list): use marquee

--- apps/web/src/app/common/blocks/keyword-list.tsx
+++ apps/web/src/app/common/blocks/keyword-list.tsx
@@ -1,14 +1,15 @@
-import { Headline } from '@nerdfish-website/ui/components'
+import { Marquee } from '@nerdfish/ui'
 import * as React from 'react'
 import { tinaField } from 'tinacms/dist/react'
 import { SectionHeader, SectionHeaderTitle } from '../components'
+import { nonNullable } from '../utils'
 import { type Block, type PageBlocksKeywordList } from '~/app/cms'
 
 const BlockLayout = ({ children }: { children: React.ReactNode }) => {
 	if (!children) return null
 
 	return (
-		<section className="py-md fle flex-col items-center">{children}</section>
+		<section className="py-md flex flex-col items-center">{children}</section>
 	)
 }
 
@@ -21,7 +22,20 @@ export function KeywordListBlock(data: Block<PageBlocksKeywordList>) {
 				<SectionHeaderTitle>{title}</SectionHeaderTitle>
 			</SectionHeader>
 			<div data-tina-field={tinaField(data, 'keywords')}>
-				<Headline>{keywords?.join(' - ') as string}</Headline>
+				<Marquee pauseOnHover duration={20000} repeat={5}>
+					{keywords?.filter(nonNullable).map((keyword) => (
+						<span
+							aria-label={keyword}
+							key={keyword}
+							className="text-7xl font-black uppercase leading-none"
+						>
+							<span>{keyword}</span>
+							<span aria-hidden className="ml-md">
+								-
+							</span>
+						</span>
+					))}
+				</Marquee>
 			</div>
 		</BlockLayout>
 	)
