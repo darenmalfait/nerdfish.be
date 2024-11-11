dcb3df95d838c4ec7fac793f269a659cde5da817
Daren Malfait <daren@nerdfish.be>
2024-11-11 07:52:49 +0100

    feat(copy): make descriptions more consice

--- apps/web/src/app/common/blocks/features.tsx
+++ apps/web/src/app/common/blocks/features.tsx
@@ -49,7 +49,7 @@ function FeatureCard(props: PageBlocksFeaturesItems) {
 		<div className="relative flex size-full flex-col items-start" {...rest}>
 			{Icon ? (
 				<div
-					className="bg-accent rounded-semi aspect-1 mb-md p-sm flex items-center justify-center text-white"
+					className="bg-accent rounded-semi aspect-1 mb-lg p-sm flex items-center justify-center text-white"
 					aria-hidden
 				>
 					<Icon
@@ -62,7 +62,7 @@ function FeatureCard(props: PageBlocksFeaturesItems) {
 				<div>
 					<H3
 						data-tina-field={tinaField(props, 'title')}
-						className="text-primary mb-sm flex flex-none items-end"
+						className="text-primary mb-md flex flex-none items-end"
 					>
 						{title}
 					</H3>
@@ -89,7 +89,7 @@ export function FeaturesBlock(props: Block<PageBlocksFeatures>) {
 				<SectionHeaderSubtitle>{subtitle}</SectionHeaderSubtitle>
 			</SectionHeader>
 			<Grid
-				className="gap-xl auto-rows-auto grid-cols-3"
+				className="gap-xl auto-rows-auto grid-cols-4"
 				data-tina-field={tinaField(props, 'items')}
 				asChild
 			>
