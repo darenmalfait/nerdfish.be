c2410042c36544880bf169214e3f2d8ca62cb7d1
Daren Malfait <daren@nerdfish.be>
2024-11-17 13:37:02 +0100

    feat(animations): use new text-slide-up component

---
+++ packages/ui/src/components/text-slide-up.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { cx } from '@nerdfish/utils'
+import { Slot } from '@radix-ui/react-slot'
+import { useInView } from 'motion/react'
+import * as React from 'react'
+
+function TextSlideUpItem({
+	children,
+	delay,
+	animate,
+}: {
+	children: React.ReactNode
+	delay?: number
+	animate?: boolean
+}) {
+	return (
+		<div className="overflow-hidden leading-[1.15]">
+			<div
+				className={cx({
+					'motion-preset-fade motion-translate-x-in-[0%] motion-translate-y-in-[100%] [animation-duration:800ms]':
+						animate,
+					invisible: !animate,
+				})}
+				style={{ animationDelay: delay ? `${delay}ms` : undefined }}
+			>
+				{children}
+			</div>
+		</div>
+	)
+}
+
+export const TextSlideUp = React.forwardRef<
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement> & {
+		staggerDelay?: number
+		asChild?: boolean
+		eager?: boolean
+	}
+>(({ children, staggerDelay = 150, asChild = false, eager, ...props }, ref) => {
+	const componentRef = React.useRef<HTMLDivElement>(null)
+	React.useImperativeHandle(ref, () => componentRef.current as HTMLDivElement)
+
+	const inView = useInView(componentRef, { once: true })
+
+	const animate = eager ?? inView
+
+	const Component = asChild ? Slot : 'div'
+	return (
+		<Component ref={componentRef} {...props}>
+			{React.Children.map(children, (child, index) => {
+				const delay = staggerDelay ? index * staggerDelay : undefined
+
+				return (
+					<TextSlideUpItem animate={animate} delay={delay}>
+						{child}
+					</TextSlideUpItem>
+				)
+			})}
+		</Component>
+	)
+})
+
+TextSlideUp.displayName = 'TextSlideUp'
