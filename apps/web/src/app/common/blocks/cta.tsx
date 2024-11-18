d041d1dbdb52f0dac47ae6e454ba40ea40fa8562
Daren Malfait <daren@nerdfish.be>
2024-11-18 10:32:52 +0100

    feat(section-header): adjust typography

--- apps/web/src/app/common/blocks/cta.tsx
+++ apps/web/src/app/common/blocks/cta.tsx
@@ -1,16 +1,11 @@
 'use client'
 
-import { Button } from '@nerdfish/ui'
+import { Button, H2 } from '@nerdfish/ui'
 import { Section } from '@nerdfish-website/ui/components'
 import { ArrowRightIcon } from '@nerdfish-website/ui/icons'
 import Link from 'next/link'
 import * as React from 'react'
 import { tinaField } from 'tinacms/dist/react'
-import {
-	SectionHeader,
-	SectionHeaderSubtitle,
-	SectionHeaderTitle,
-} from '../components'
 import { getPagePath } from '~/app/[locale]/(pages)/utils'
 import { type PageBlocksCta, type Block } from '~/app/cms'
 
@@ -26,14 +21,19 @@ export function CtaBlock(props: Block<PageBlocksCta>) {
 	return (
 		<BlockLayout>
 			<div className="gap-lg flex flex-col justify-between md:flex-row md:items-center">
-				<SectionHeader className="!mb-0">
-					<SectionHeaderTitle data-tina-field={tinaField(props, 'title')}>
-						{title}
-					</SectionHeaderTitle>
-					<SectionHeaderSubtitle data-tina-field={tinaField(props, 'subtitle')}>
-						{subtitle}
-					</SectionHeaderSubtitle>
-				</SectionHeader>
+				<div className="space-y-md flex flex-col lg:flex-row lg:items-end lg:justify-between lg:space-y-0">
+					<div className="space-y-sm">
+						<H2 data-tina-field={tinaField(props, 'title')}>{title}</H2>
+						<H2
+							variant="secondary"
+							as="div"
+							data-tina-field={tinaField(props, 'subtitle')}
+						>
+							{subtitle}
+						</H2>
+					</div>
+				</div>
+
 				{page?._sys.breadcrumbs ? (
 					<div>
 						<Button
