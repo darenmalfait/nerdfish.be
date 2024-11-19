9d2026a9a1ac5ad341018a1083fff3bd5825bbfd
Daren Malfait <daren@nerdfish.be>
2024-11-19 15:31:58 +0100

    feat(global): remove trailing slash config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	trailingSlash: true,
+	trailingSlash: false,
 	swcMinify: true,
 	images: {
 		remotePatterns: [{ hostname: 'assets.tina.io' }],
