fcbe0335428bfc720f406d526c405e1d456bfdd0
Daren Malfait <daren@nerdfish.be>
2024-11-25 08:31:03 +0100

    feat(footer): remove navigation

--- apps/web/src/app/layout/site-layout.tsx
+++ apps/web/src/app/layout/site-layout.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { LoadingAnimation, H1, H3, Separator } from '@nerdfish/ui'
+import { LoadingAnimation, H1, Separator } from '@nerdfish/ui'
 import { cx } from '@nerdfish/utils'
-import { stripPreSlash } from '@nerdfish-website/lib/utils'
 import {
 	Section,
 	InViewBackground,
@@ -10,89 +9,10 @@ import {
 } from '@nerdfish-website/ui/components'
 import { ArrowRight } from '@nerdfish-website/ui/icons'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import * as React from 'react'
 import { SocialLinks } from './navigation'
 import { SiteHeader } from './site-header'
-import {
-	type GlobalNavigationMain,
-	type GlobalNavigationMainSub,
-} from '~/app/cms'
 import { useGlobal } from '~/app/global-provider'
-
-const FooterNavigationSubItem = React.forwardRef<
-	React.ElementRef<typeof Link>,
-	React.ComponentPropsWithoutRef<typeof Link> & GlobalNavigationMainSub
->(({ href, label, description, className, ...props }, ref) => {
-	const pathname = usePathname()
-
-	const isActive = stripPreSlash(pathname).startsWith(stripPreSlash(href))
-
-	return (
-		<li>
-			<Link ref={ref} href={`/${stripPreSlash(href)}`} {...props}>
-				<span
-					className={cx(
-						'hover:text-accent text-muted border-b-4 border-transparent capitalize',
-						isActive && 'border-b-accent',
-					)}
-				>
-					{label}
-				</span>
-			</Link>
-		</li>
-	)
-})
-FooterNavigationSubItem.displayName = 'FooterNavigationSubItem'
-
-const FooterNavigationItem = React.forwardRef<
-	React.ElementRef<typeof Link>,
-	Omit<React.ComponentPropsWithoutRef<typeof Link>, 'href'> &
-		GlobalNavigationMain
->(({ href, label, sub, ...props }, ref) => {
-	const pathname = usePathname()
-
-	if (!sub?.length && !href) return null
-
-	if (!sub?.length) {
-		const isActive = stripPreSlash(pathname).startsWith(
-			stripPreSlash(href ?? ''),
-		)
-
-		return (
-			<Link href={`/${stripPreSlash(href ?? '')}`} ref={ref} {...props}>
-				<H3
-					className={cx(
-						'hover:text-accent border-b-4 border-transparent capitalize',
-						isActive && 'border-b-accent',
-					)}
-					variant="primary"
-					as="span"
-				>
-					{label}
-				</H3>
-			</Link>
-		)
-	}
-
-	return (
-		<div>
-			<H3 className="capitalize" variant="primary" as="span">
-				{label}
-			</H3>
-			<ul className="mt-md gap-sm grid">
-				{sub.map((subNavItem) => {
-					if (!subNavItem) return null
-
-					return (
-						<FooterNavigationSubItem key={subNavItem.label} {...subNavItem} />
-					)
-				})}
-			</ul>
-		</div>
-	)
-})
-FooterNavigationItem.displayName = 'FooterNavigationItem'
 
 function Disclaimer() {
 	const { companyInfo } = useGlobal()
@@ -133,7 +53,7 @@ function SiteFooterHeading() {
 	const { paths } = useGlobal()
 
 	return (
-		<H1 as="h2" className="mb-2xl text-primary">
+		<H1 as="h2" className="py-lg text-primary">
 			<Link
 				href={paths?.contact ?? '/'}
 				className="group"
@@ -155,29 +75,14 @@ const SiteFooter = React.forwardRef<
 	HTMLDivElement,
 	React.ComponentPropsWithoutRef<'div'>
 >(({ className, ...props }, ref) => {
-	const { navigation } = useGlobal()
-
 	return (
-		<div ref={ref} className={cx('py-lg mt-lg', className)} {...props}>
+		<div ref={ref} className={cx('py-lg pb-xl mt-lg', className)} {...props}>
 			<InViewBackground className="bg-secondary">
 				<footer
 					className="text-primary px-lg mx-auto"
 					aria-labelledby="footer-heading"
 				>
 					<SiteFooterHeading />
-					<nav className="pb-lg mx-auto">
-						<div className="gap-lg xl:grid">
-							<div className="mt-lg gap-lg xsm:grid-cols-2 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-								{navigation?.main?.map((navItem) => {
-									if (!navItem) return null
-
-									return (
-										<FooterNavigationItem key={navItem.label} {...navItem} />
-									)
-								})}
-							</div>
-						</div>
-					</nav>
 					<Separator className="my-lg" />
 					<Disclaimer />
 				</footer>
