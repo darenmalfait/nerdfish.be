02c604c0045d16cbe31e1f89882cfecfa47d2e71
Daren Malfait <daren@nerdfish.be>
2024-11-26 17:24:14 +0100

    feat(contact-form): make one of contact fields required

--- apps/web/src/app/[locale]/contact/components/contact-form/validation.ts
+++ apps/web/src/app/[locale]/contact/components/contact-form/validation.ts
@@ -12,29 +12,46 @@ const phoneRegex = new RegExp(
 	/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/,
 )
 
-export const contactSchema = z.object({
-	name: z
-		.string()
-		.min(2, 'Your name seems a bit short.')
-		.max(32, 'Your name seems a bit long.'),
-	company: optionalField(
-		z
+export const contactSchema = z
+	.object({
+		name: z
 			.string()
-			.min(2, 'Your company name seems a bit short.')
-			.max(32, 'Your company name seems a bit long.'),
-	),
-	email: z.string().email('Please enter a valid email address.'),
-	phone: optionalField(z.string().regex(phoneRegex, 'Invalid Number!')),
-	textMessage: z
-		.string()
-		.min(10, 'I hope that your message is a bit longer than that.')
-		.max(
-			512,
-			'I like that you have a lot to say, but please keep it under 512 characters.',
+			.min(2, 'Your name seems a bit short.')
+			.max(32, 'Your name seems a bit long.'),
+		company: optionalField(
+			z
+				.string()
+				.min(2, 'Your company name seems a bit short.')
+				.max(32, 'Your company name seems a bit long.'),
 		),
-	projectType: z.array(z.enum(projectTypes)),
-	budgetRange: z.array(z.number()).optional(),
-	recaptchaResponse: z.string().optional(),
-})
+		email: optionalField(
+			z.string().email('Please enter a valid email address.'),
+		),
+		phone: optionalField(z.string().regex(phoneRegex, 'Invalid Number!')),
+		textMessage: z
+			.string()
+			.min(10, 'I hope that your message is a bit longer than that.')
+			.max(
+				512,
+				'I like that you have a lot to say, but please keep it under 512 characters.',
+			),
+		projectType: z.array(z.enum(projectTypes)),
+		budgetRange: z.array(z.number()).optional(),
+		recaptchaResponse: z.string().optional(),
+	})
+	.superRefine(({ phone, email }, ctx) => {
+		if (email === undefined && phone === undefined) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				path: ['email'],
+				message: 'Please provide either an email or phone number.',
+			})
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				path: ['phone'],
+				message: 'Please provide either an email or phone number.',
+			})
+		}
+	})
 
 export type ContactFormData = z.infer<typeof contactSchema>
