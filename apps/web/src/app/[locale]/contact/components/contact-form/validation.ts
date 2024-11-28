4e973406823201dc3a4388e6abec9afa2025b60e
Daren Malfait <daren@nerdfish.be>
2024-11-28 18:55:25 +0100

    feat(contact-form): improve partial validation

--- apps/web/src/app/[locale]/contact/components/contact-form/validation.ts
+++ apps/web/src/app/[locale]/contact/components/contact-form/validation.ts
@@ -12,46 +12,46 @@ const phoneRegex = new RegExp(
 	/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
 )
 
-export const contactSchema = z
-	.object({
-		name: z
+export const contactSchema = z.object({
+	name: z
+		.string()
+		.min(2, 'Your name seems a bit short.')
+		.max(32, 'Your name seems a bit long.'),
+	company: optionalField(
+		z
 			.string()
-			.min(2, 'Your name seems a bit short.')
-			.max(32, 'Your name seems a bit long.'),
-		company: optionalField(
-			z
-				.string()
-				.min(2, 'Your company name seems a bit short.')
-				.max(32, 'Your company name seems a bit long.')
-		),
-		email: optionalField(
-			z.string().email('Please enter a valid email address.')
-		),
-		phone: optionalField(z.string().regex(phoneRegex, 'Invalid Number!')),
-		textMessage: z
-			.string()
-			.min(10, 'I hope that your message is a bit longer than that.')
-			.max(
-				512,
-				'I like that you have a lot to say, but please keep it under 512 characters.'
+			.min(2, 'Your company name seems a bit short.')
+			.max(32, 'Your company name seems a bit long.')
+	),
+	contact: z
+		.object({
+			email: optionalField(
+				z.string().email('Please enter a valid email address.')
 			),
-		projectType: z.array(z.enum(projectTypes)),
-		budgetRange: z.array(z.number()).optional(),
-		recaptchaResponse: z.string().optional(),
-	})
-	.superRefine(({ phone, email }, ctx) => {
-		if (email === undefined && phone === undefined) {
-			ctx.addIssue({
-				code: z.ZodIssueCode.custom,
-				path: ['email'],
-				message: 'Please provide either an email or phone number.',
-			})
-			ctx.addIssue({
-				code: z.ZodIssueCode.custom,
-				path: ['phone'],
-				message: 'Please provide either an email or phone number.',
-			})
-		}
-	})
+			phone: optionalField(
+				z.string().regex(phoneRegex, 'This is not a valid phone number.')
+			),
+		})
+		.partial()
+		.superRefine((data, ctx) => {
+			if (!data.email?.length && !data.phone?.length) {
+				ctx.addIssue({
+					path: ['email'],
+					code: z.ZodIssueCode.custom,
+					message: 'Either email or phone should be filled in.',
+				})
+			}
+		}),
+	textMessage: z
+		.string()
+		.min(10, 'I hope that your message is a bit longer than that.')
+		.max(
+			512,
+			'I like that you have a lot to say, but please keep it under 512 characters.'
+		),
+	projectType: z.array(z.enum(projectTypes)),
+	budgetRange: z.array(z.number()).optional(),
+	recaptchaResponse: z.string().optional(),
+})
 
 export type ContactFormData = z.infer<typeof contactSchema>
