0fab3edfd8dda3ecfe0cb510f2f1a92c19c22b12
Daren Malfait <daren@nerdfish.be>
2024-11-28 23:16:11 +0100

    refactor(get-translations): get translation based on locale

--- apps/web/src/app/i18n/get-translations.ts
+++ apps/web/src/app/i18n/get-translations.ts
@@ -1,18 +1,14 @@
 import 'server-only'
-
-import { i18n } from '~/app/i18n/config'
 import type { Dictionary, Locale } from '~/app/i18n/types'
 
-// We enumerate all dictionaries here for better linting and typescript support
-// We also get the default import for cleaner types
-const dictionaries: Record<Locale, () => Promise<Dictionary>> = {
-	en: () => import('./dictionaries/en.json').then((module) => module.default),
-	nl: () => import('./dictionaries/nl.json').then((module) => module.default),
-}
-
 export const getTranslations = async (locale: Locale): Promise<Dictionary> => {
-	const dict = dictionaries[locale]?.() ?? dictionaries[i18n.defaultLocale]?.()
-	if (!dict) throw new Error('Dictionary not found')
+	const common = await import(`./dictionaries/${locale}.json`).then(
+		(module) => module.default
+	)
 
-	return dict
+	if (!common) throw new Error('Dictionary not found')
+
+	return {
+		...common,
+	}
 }
