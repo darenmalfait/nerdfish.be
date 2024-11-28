98243e2d36084cd7453900e5b94fe5f2de9695f7
Daren Malfait <daren@nerdfish.be>
2024-11-28 17:44:40 +0100

    feat(blogs): add back to blog translation

--- apps/web/src/app/[locale]/blog/components/misc.tsx
+++ apps/web/src/app/[locale]/blog/components/misc.tsx
@@ -4,8 +4,10 @@ import { Button } from '@nerdfish/ui'
 import { ChevronLeftIcon } from '@repo/ui/icons'
 import Link from 'next/link'
 import { useGlobal } from '~/app/global-provider'
+import { useTranslation } from '~/app/i18n/translation-provider'
 
 export function BackToBlog() {
+	const { t } = useTranslation()
 	const { paths } = useGlobal()
 
 	if (!paths?.blog) return null
@@ -14,7 +16,7 @@ export function BackToBlog() {
 		<Button variant="outline" asChild>
 			<Link href={paths.blog}>
 				<ChevronLeftIcon className="mr-2 size-4" />
-				All blog articles
+				{t('global.allArticles')}
 			</Link>
 		</Button>
 	)
