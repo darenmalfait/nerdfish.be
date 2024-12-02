28e4a52c6c1bb0c0a952ed549195efb71a873b18
Daren Malfait <daren@nerdfish.be>
2024-12-02 21:21:05 +0100

    refactor(i18n): cleanup code

--- apps/web/src/app/i18n/global.d.ts
+++ apps/web/src/app/i18n/global.d.ts
@@ -1,8 +1,6 @@
 // Use type safe message keys with `next-intl`
-import type GlobalTranslations from './dictionaries/en.json'
-import type ZodTranslations from './dictionaries/zod/en.json'
+import type GlobalMessages from './dictionaries/en.json'
+import type ZodMessages from './dictionaries/zod/en.json'
 
-type GlobalMessages = typeof GlobalTranslations
-type ZodMessages = typeof ZodTranslations
-type Messages = GlobalMessages & ZodMessages
+type Messages = typeof GlobalMessages & typeof ZodMessages
 type IntlMessages = Messages
