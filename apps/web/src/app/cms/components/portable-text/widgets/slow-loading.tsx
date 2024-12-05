41342d7e9d9e3ef29962b0a04bca09bd9c5e653f
Daren Malfait <daren@nimblestudio.com>
2024-12-05 23:05:13 +0100

    feat(blog): add widgets in portable text

---
+++ apps/web/src/app/cms/components/portable-text/widgets/slow-loading.tsx
@@ -0,0 +1,36 @@
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	LoadingAnimation,
+	Skeleton,
+} from '@nerdfish/ui'
+import { useTranslations } from 'next-intl'
+import React from 'react'
+
+export function SlowLoading() {
+	const t = useTranslations('widgets.slowLoading')
+
+	return (
+		<div className="gap-md py-2xl flex flex-col">
+			<Card className="bg-muted mx-auto w-full max-w-sm">
+				<CardHeader>
+					<div className="relative flex h-36 w-full items-center justify-center">
+						<Skeleton className="absolute inset-0 h-36 w-full object-cover" />
+						<LoadingAnimation variant="classic" />
+					</div>
+				</CardHeader>
+				<CardContent>
+					<div className="gap-sm flex flex-col">
+						<Skeleton className="h-3 w-1/2" />
+						<Skeleton className="h-3 w-3/4" />
+						<Skeleton className="h-3 w-1/3" />
+					</div>
+				</CardContent>
+			</Card>
+			<caption className="text-muted-foreground text-sm">
+				{t('caption')}
+			</caption>
+		</div>
+	)
+}
