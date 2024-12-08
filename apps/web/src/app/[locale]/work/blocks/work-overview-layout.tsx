e424f56c327df607c29c0bccef973eadd6264c4e
Daren Malfait <daren@nimblestudio.com>
2024-12-08 07:37:17 +0100

    fix(article-search-input): add label to input

--- apps/web/src/app/[locale]/work/blocks/work-overview-layout.tsx
+++ apps/web/src/app/[locale]/work/blocks/work-overview-layout.tsx
@@ -64,7 +64,7 @@ export function BlockLayout({
 							alt: header?.title ?? undefined,
 						}}
 					/>
-					<ArticleOverviewSearchContent>
+					<ArticleOverviewSearchContent inputLabel={t('search')}>
 						<SectionHeader>
 							<SectionHeaderTitle>{header?.title}</SectionHeaderTitle>
 							<SectionHeaderSubtitle>
