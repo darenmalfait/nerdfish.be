9eb67b2510645eb1951afe34e34fd555e9853444
Daren Malfait <daren@nerdfish.be>
2024-12-08 07:37:17 +0100

    fix(article-search-input): add label to input

--- apps/web/src/app/[locale]/blog/blocks/blog-overview-layout.tsx
+++ apps/web/src/app/[locale]/blog/blocks/blog-overview-layout.tsx
@@ -64,7 +64,7 @@ export function BlockLayout({
 							alt: header?.title ?? undefined,
 						}}
 					/>
-					<ArticleOverviewSearchContent>
+					<ArticleOverviewSearchContent inputLabel={t('search')}>
 						<SectionHeader>
 							<H1 variant="primary" className="mb-lg">
 								{header?.title}
