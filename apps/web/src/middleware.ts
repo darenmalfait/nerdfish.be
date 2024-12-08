1f198cc275e505f0e5c1ed7c098ec2567948e553
Daren Malfait <daren@nimblestudio.com>
2024-12-08 19:59:33 +0100

    feat(middleware): add csp

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,5 +1,65 @@
-import createMiddleware from 'next-intl/middleware'
-import { i18n } from './app/i18n/config'
+import {
+	type NextFetchEvent,
+	type NextMiddleware,
+	type NextRequest,
+	NextResponse,
+} from 'next/server'
+import { withIntl } from './app/i18n/middleware'
+import { type MiddlewareFactory } from './app/types'
+
+function stackMiddlewares(
+	functions: MiddlewareFactory[] = [],
+	index = 0,
+): NextMiddleware {
+	const current = functions[index]
+	if (current) {
+		const next = stackMiddlewares(functions, index + 1)
+		return current(next)
+	}
+	return () => NextResponse.next()
+}
+
+const withCSP: MiddlewareFactory = (next: NextMiddleware) => {
+	return async (request: NextRequest, _next: NextFetchEvent) => {
+		if (request.headers.get('x-nonce') != null) {
+			const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+			const cspHeader = `
+    	default-src 'self';
+    	script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    	style-src 'self' 'nonce-${nonce}';
+    	img-src 'self' blob: data:;
+    	font-src 'self';
+    	object-src 'none';
+			base-uri 'self';
+			form-action 'self';
+			frame-ancestors 'none';
+			upgrade-insecure-requests;
+			`
+
+			// Replace newline characters and spaces
+			const contentSecurityPolicyHeaderValue = cspHeader
+				.replace(/\s{2,}/g, ' ')
+				.trim()
+			request.headers.set(
+				'Content-Security-Policy',
+				contentSecurityPolicyHeaderValue,
+			)
+
+			const response = await next(request, _next)
+			if (response) {
+				response.headers.set(
+					'Content-Security-Policy',
+					contentSecurityPolicyHeaderValue,
+				)
+				response.headers.set('x-nonce', nonce)
+			}
+
+			return response
+		} else {
+			return next(request, _next)
+		}
+	}
+}
 
 export const config = {
 	// Matcher ignoring `/_next/` and `/api/`
@@ -8,13 +68,5 @@ export const config = {
 	],
 }
 
-export default createMiddleware({
-	locales: i18n.locales,
-	defaultLocale: i18n.defaultLocale,
-	localePrefix: 'as-needed',
-	localeDetection: true,
-	localeCookie: {
-		secure: true,
-		sameSite: 'strict',
-	},
-})
+const middlewares = [withIntl, withCSP]
+export default stackMiddlewares(middlewares)
