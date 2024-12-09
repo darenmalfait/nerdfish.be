ba1db641fd2d16e9bbeb953f53c8b8d3fd974488
Daren Malfait <daren@nimblestudio.com>
2024-12-09 07:25:34 +0100

    fix(middleware): remove csp that doesn't work

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,85 +1,29 @@
-import {
-	type NextFetchEvent,
-	type NextMiddleware,
-	type NextRequest,
-	NextResponse,
-} from 'next/server'
-import { withIntl } from './app/i18n/middleware'
-import { type MiddlewareFactory } from './app/types'
-
-function stackMiddlewares(
-	functions: MiddlewareFactory[] = [],
-	index = 0,
-): NextMiddleware {
-	const current = functions[index]
-	if (current) {
-		const next = stackMiddlewares(functions, index + 1)
-		return current(next)
-	}
-	return () => NextResponse.next()
-}
-
-const withCSP: MiddlewareFactory = (next: NextMiddleware) => {
-	return async (request: NextRequest, _next: NextFetchEvent) => {
-		const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
-
-		const cspHeader = `
-    		default-src 'self';
-    		script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    		style-src 'self' 'nonce-${nonce}';
-    		img-src 'self' blob: data:;
-    		font-src 'self';
-    		object-src 'none';
-    		base-uri 'self';
-    		form-action 'self';
-    		frame-ancestors 'none';
-    		upgrade-insecure-requests;
-			`
-		// Replace newline characters and spaces
-		const contentSecurityPolicyHeaderValue = cspHeader
-			.replace(/\s{2,}/g, ' ')
-			.trim()
-
-		const requestHeaders = new Headers(request.headers)
-		requestHeaders.set('x-nonce', nonce)
-
-		requestHeaders.set(
-			'Content-Security-Policy',
-			contentSecurityPolicyHeaderValue,
-		)
-
-		const response = await next(request, _next)
-
-		response?.headers.set(
-			'Content-Security-Policy',
-			contentSecurityPolicyHeaderValue,
-		)
-
-		return response
-	}
-}
+import createMiddleware from 'next-intl/middleware'
+import { i18n } from './app/i18n/config'
 
 export const config = {
+	/*
+	 * Match all request paths except for the ones starting with:
+	 * - api (API routes)
+	 * - _next/static (static files)
+	 * - _next/image (image optimization files)
+	 * - favicon.ico (favicon file)
+	 * - folders in public (which resolve to /foldername)
+	 * - sitemap.xml
+	 * - robots.txt
+	 */
 	matcher: [
-		/*
-		 * Match all request paths except for the ones starting with:
-		 * - api (API routes)
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico (favicon file)
-		 * - public folders
-		 * - sitemap.xml
-		 * - robots.txt
-		 */
-		{
-			source:
-				'/((?!api|_next/static|_next/image|uploads|images|admin|favicon.ico|sitemap.xml|robots.txt).*)',
-			missing: [
-				{ type: 'header', key: 'next-router-prefetch' },
-				{ type: 'header', key: 'purpose', value: 'prefetch' },
-			],
-		},
+		'/((?!api|_next/static|_next/image|uploads|images|admin|favicon.ico|sitemap.xml|robots.txt).*)',
 	],
 }
 
-export default stackMiddlewares([withIntl, withCSP])
+export default createMiddleware({
+	locales: i18n.locales,
+	defaultLocale: i18n.defaultLocale,
+	localePrefix: 'as-needed',
+	localeDetection: true,
+	localeCookie: {
+		secure: true,
+		sameSite: 'strict',
+	},
+})
