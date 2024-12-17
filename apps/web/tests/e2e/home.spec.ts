9b2c564179de3a31c98fe00b3594a331f207eaf7
Daren Malfait <56068461+darenmalfait@users.noreply.github.com>
2024-12-17 07:16:16 +0100

    feat(web): add a11y checks (#415)

---
+++ apps/web/tests/e2e/home.spec.ts
@@ -0,0 +1,47 @@
+import AxeBuilder from '@axe-core/playwright'
+import { expect, test } from '@playwright/test'
+import A11yError from 'playwright/a11y-logger'
+import { A11YTAGS } from './constants'
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('/')
+})
+
+test.describe('homepage a11y', () => {
+	test('homepage should not have any automatically detectable WCAG A or AA violations in light mode', async ({
+		page,
+	}) => {
+		await page.getByLabel('Switch to Light theme').click()
+
+		await expect(
+			page.getByRole('heading', { level: 1, name: 'Nerdfish' }),
+		).toBeVisible()
+
+		const a11yScanResults = await new AxeBuilder({ page })
+			.withTags(A11YTAGS)
+			.analyze()
+		if (a11yScanResults.violations.length > 0) {
+			throw new A11yError(a11yScanResults.violations)
+		}
+		expect(a11yScanResults.violations).toEqual([])
+	})
+
+	test('homepage should not have any automatically detectable WCAG A or AA violations in dark Mode', async ({
+		page,
+	}) => {
+		await page.getByLabel('Switch to Dark theme').click()
+
+		await expect(
+			page.getByRole('heading', { level: 1, name: 'Nerdfish' }),
+		).toBeVisible()
+
+		const a11yScanResults = await new AxeBuilder({ page })
+			.withTags(A11YTAGS)
+			.analyze()
+
+		if (a11yScanResults.violations.length > 0) {
+			throw new A11yError(a11yScanResults.violations)
+		}
+		expect(a11yScanResults.violations).toEqual([])
+	})
+})
