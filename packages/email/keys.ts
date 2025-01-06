30d8f7c8ea8ad38e64da432d864857574658f7af
Daren Malfait <daren@nerdfish.be>
2025-01-06 20:13:49 +0100

    feat(email): coerce skip email to boolean

--- packages/email/keys.ts
+++ packages/email/keys.ts
@@ -6,7 +6,13 @@ export const keys = () =>
 		server: {
 			RESEND_API_KEY: z.string().min(1).startsWith('re_'),
 			EMAIL_FROM: z.string().min(1),
-			SKIP_EMAILS: z.boolean().optional(),
+			SKIP_EMAILS: z.preprocess((val) => {
+				if (typeof val === 'string') {
+					if (['1', 'true'].includes(val.toLowerCase())) return true
+					if (['0', 'false'].includes(val.toLowerCase())) return false
+				}
+				return false
+			}, z.coerce.boolean()),
 		},
 		runtimeEnv: {
 			RESEND_API_KEY: process.env.RESEND_API_KEY,
