34914f6eb8e9227c671f0f723999338dc032b247
Daren Malfait <daren@nerdfish.be>
2025-01-06 10:01:00 +0100

    feat(sitemap): improve sitemap with alternate languages

--- apps/web/routing.ts
+++ apps/web/routing.ts
@@ -1,29 +1,35 @@
 import { i18n } from '@repo/i18n/config'
 import { createNavigation, defineRouting } from '@repo/i18n/routing'
 
+export const basePathNames = {
+	'/': '/',
+	'/3d-printing': '/3d-printing',
+	'/ai': '/ai',
+	'/about': {
+		en: '/about',
+		nl: '/over-mij',
+	},
+	'/blog': '/blog',
+	'/branding': '/branding',
+	'/contact': '/contact',
+	'/freelance': '/freelance',
+	'/uxui-design': '/uxui-design',
+	'/webdesign': '/webdesign',
+	'/wiki': '/wiki',
+	'/work': {
+		en: '/work',
+		nl: '/werk',
+	},
+}
+
 export const routing = defineRouting({
 	locales: i18n.locales,
 	defaultLocale: i18n.defaultLocale,
 	localePrefix: 'as-needed',
 	pathnames: {
-		'/': '/',
-		'/3d-printing': '/3d-printing',
-		'/about': {
-			en: '/about',
-			nl: '/over-mij',
-		},
-		'/blog': '/blog',
+		...basePathNames,
 		'/blog/:slug': '/blog/:slug',
-		'/branding': '/branding',
-		'/contact': '/contact',
-		'/freelance': '/freelance',
-		'/uxui-design': '/uxui-design',
-		'/webdesign': '/webdesign',
-		'/wiki': '/wiki',
-		'/work': {
-			en: '/work',
-			nl: '/werk',
-		},
+		'/tooling/*': '/tooling/*',
 		'/work/:slug': '/work/:slug',
 
 		// template for when it differs per locale
