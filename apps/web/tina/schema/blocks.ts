36567a3d636c7ff1612df3be6a439935e15af995
Daren Malfait <daren@nerdfish.be>
2025-01-07 21:51:18 +0100

    feat(web): remove some obsolete code

--- apps/web/tina/schema/blocks.ts
+++ apps/web/tina/schema/blocks.ts
@@ -6,12 +6,9 @@ import { ctaTemplate } from './blocks/cta.template'
 import { faqTemplate } from './blocks/faq.template'
 import { featuresTemplate } from './blocks/features.template'
 import { heroTemplate } from './blocks/hero.template'
-import { highlightTemplate } from './blocks/highlight.template'
-import { highlightsTemplate } from './blocks/highlights.template'
 import { imageGridTemplate } from './blocks/image-grid.template'
 import { keywordListTemplate } from './blocks/keyword-list.template'
 import { pricingTemplate } from './blocks/pricing.template'
-import { productsTemplate } from './blocks/products.template'
 import { skillsTemplate } from './blocks/skills.template'
 import { testimonialsTemplate } from './blocks/testimonials.template'
 import { workTemplate } from './blocks/work.template'
@@ -25,12 +22,9 @@ export const blocks = [
 	faqTemplate,
 	featuresTemplate,
 	heroTemplate,
-	highlightTemplate,
-	highlightsTemplate,
 	imageGridTemplate,
 	keywordListTemplate,
 	pricingTemplate,
-	productsTemplate,
 	skillsTemplate,
 	workTemplate,
 	testimonialsTemplate,
