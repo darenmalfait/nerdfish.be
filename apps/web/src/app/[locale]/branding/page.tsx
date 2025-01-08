3517f08fc7fd74426183662c9bd69aa8108f7ac9
Daren Malfait <daren@nerdfish.be>
2025-01-08 11:17:46 +0100

    refactor(branding): move to page

---
+++ apps/web/src/app/[locale]/branding/page.tsx
@@ -0,0 +1,89 @@
+import {
+	Section,
+	SectionHeader,
+	SectionHeaderSubtitle,
+	SectionHeaderTitle,
+} from '@repo/design-system/components/section'
+import { i18n } from '@repo/i18n/config'
+import { getTranslations } from '@repo/i18n/server'
+import { type WithLocale } from '@repo/i18n/types'
+import { createMetadata } from '@repo/seo/metadata'
+import { type Metadata } from 'next'
+import { getPathname, getPathnames } from 'routing'
+import { Features } from '../../components/features'
+import {
+	Hero,
+	HeroContent,
+	HeroSubtitle,
+	HeroTitle,
+} from '../../components/hero'
+
+type PageProps = {
+	params: Promise<WithLocale>
+}
+
+export async function generateMetadata(
+	props: PageProps,
+): Promise<Metadata | undefined> {
+	const { locale } = await props.params
+	const t = await getTranslations('pages.branding')
+
+	return createMetadata({
+		title: t('_meta.title'),
+		description: t('_meta.description'),
+		alternates: {
+			canonical: getPathname({ locale, href: '/branding' }),
+			languages: getPathnames(
+				'/branding',
+				i18n.locales.filter((l) => l !== locale),
+			),
+		},
+		locale,
+	})
+}
+
+export default async function BrandingPage(props: {
+	params: Promise<WithLocale>
+}) {
+	await props.params
+	const t = await getTranslations('pages.branding')
+
+	return (
+		<>
+			<Hero variant="secondary">
+				<HeroContent>
+					<HeroTitle title={t('hero.title')} />
+					<HeroSubtitle>{t('hero.subtitle')}</HeroSubtitle>
+				</HeroContent>
+			</Hero>
+
+			<Section>
+				<SectionHeader>
+					<SectionHeaderTitle>{t('features.title')}</SectionHeaderTitle>
+					<SectionHeaderSubtitle>
+						{t('features.subtitle')}
+					</SectionHeaderSubtitle>
+				</SectionHeader>
+				<Features
+					items={[
+						{
+							title: t('features.items.0.title'),
+							description: t('features.items.0.description'),
+							icon: 'Palette',
+						},
+						{
+							title: t('features.items.1.title'),
+							description: t('features.items.1.description'),
+							icon: 'Layers',
+						},
+						{
+							title: t('features.items.2.title'),
+							description: t('features.items.2.description'),
+							icon: 'Users',
+						},
+					]}
+				/>
+			</Section>
+		</>
+	)
+}
