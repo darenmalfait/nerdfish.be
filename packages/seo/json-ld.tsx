ef0e97c470e6b8d82341df6bc13927aece152dcb
Daren Malfait <daren@nerdfish.be>
2025-01-17 13:38:09 +0100

    fix(seo): sanitize script

--- packages/seo/json-ld.tsx
+++ packages/seo/json-ld.tsx
@@ -1,10 +1,15 @@
+import DOMPurify from 'dompurify'
 import { type Thing, type WithContext } from 'schema-dts'
 
-export const JsonLd = ({ code }: { code: WithContext<Thing> }) => (
-	<script
-		type="application/ld+json"
-		dangerouslySetInnerHTML={{ __html: JSON.stringify(code) }}
-	/>
-)
+export const JsonLd = ({ code }: { code: WithContext<Thing> }) => {
+	const cleanCode = DOMPurify.sanitize(JSON.stringify(code))
+
+	return (
+		<script
+			type="application/ld+json"
+			dangerouslySetInnerHTML={{ __html: cleanCode }}
+		/>
+	)
+}
 
 export type * from 'schema-dts'
