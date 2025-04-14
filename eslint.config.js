dbb44a979af3993654218db12ea3f30d9f204ad7
Daren Malfait <daren@nerdfish.be>
2025-04-14 15:23:30 +0200

    chore(app): update @nerdfish-config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,25 @@ import { config as defaultConfig } from '@nerdfish/config/eslint'
 export default [
 	...defaultConfig,
 	{
+		files: ['**/*.ts?(x)', '**/*.js?(x)'],
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+		plugins: {
+			'@next/next': (await import('@next/eslint-plugin-next')).default,
+		},
+		rules: {
+			...(await import('@next/eslint-plugin-next')).default.configs.recommended
+				.rules,
+			...(await import('@next/eslint-plugin-next')).default.configs[
+				'core-web-vitals'
+			].rules,
+			'@next/next/no-img-element': 'error',
+		},
+	},
+	{
 		ignores: [
 			'**/__generated__/**',
 			'**/.next/**',
