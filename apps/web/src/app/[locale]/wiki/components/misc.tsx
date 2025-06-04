5a81c9a36dfa99054495d3809bea3f1a67759172
Daren Malfait <daren@nerdfish.be>
2025-06-04 11:51:48 +0200

    fix(articles): use link as back button style

--- apps/web/src/app/[locale]/wiki/components/misc.tsx
+++ apps/web/src/app/[locale]/wiki/components/misc.tsx
@@ -9,7 +9,7 @@ export function BackToWiki() {
 	const t = useTranslations('wiki.overview')
 
 	return (
-		<Button variant="ghost" className="group -mx-[1.5rem]" asChild>
+		<Button variant="link" className="group -mx-[1.5rem]" asChild>
 			<Link href="/wiki">
 				<ChevronLeftIcon className="group-hover:-translate-x-xs mr-sm size-4 transition-transform" />
 				{t('backToWiki')}
