976d60ca1c97e9d23bb24675ee480705e82bd59e
Daren Malfait <daren@nerdfish.be>
2025-06-05 12:04:03 +0200

    fix(wiki): improve filtering

--- apps/web/src/app/[locale]/wiki/utils.ts
+++ apps/web/src/app/[locale]/wiki/utils.ts
@@ -8,32 +8,20 @@ import { getCrypto } from '../tooling/(timesheets)/timesheets/utils'
 export function filterWiki(posts: PartialDeep<Wiki>[], searchString: string) {
 	if (!searchString) return posts
 
-	const fuse = new Fuse(posts, {
-		keys: ['title', 'tags', 'excerpt'],
-		minMatchCharLength: 1,
-		threshold: 0.3,
-	})
-
-	const allResults = fuse.search(searchString).map((result) => result.item)
-
-	const words = new Set(searchString.split(' '))
-
-	// if there's only one word then we're done
-	if (words.size < 2) return allResults
-
-	// if there are multiple words, we'll conduct an individual search for each word
-	// and then combine the results
-	const individualWordResults = new Set<PartialDeep<Wiki>>()
+	const words = searchString.split(' ')
+	let results = posts
 
 	for (const word of words) {
-		const items = fuse.search(word).map((result) => result.item)
-
-		for (const item of items) {
-			individualWordResults.add(item)
-		}
+		const fuse = new Fuse(results, {
+			keys: ['title', 'tags', 'excerpt'],
+			isCaseSensitive: false,
+			minMatchCharLength: 1,
+			threshold: 0.3,
+		})
+		results = fuse.search(word).map((result) => result.item)
 	}
 
-	return Array.from(new Set([...allResults, ...individualWordResults]))
+	return results
 }
 
 export function getWikiPath(post: PartialDeep<Wiki>) {
