543165037450883673e92f90ec314b88fe53ed28
Daren Malfait <daren@nerdfish.be>
2025-06-17 08:36:08 +0200

    feat(products): add basic pages

---
+++ apps/web/src/app/[locale]/product/utils.ts
@@ -0,0 +1,12 @@
+import { i18n } from '@repo/i18n/config'
+import { type PartialDeep } from '@repo/lib/types'
+import { type Product } from 'content-collections'
+
+export function getProductPath(post: PartialDeep<Product>) {
+	const locale = post.locale
+
+	const localePath = locale === i18n.defaultLocale ? '' : `/${locale}`
+	const blogPath = `/product/${post.slug}`
+
+	return `${localePath}${blogPath}`
+}
--- packages/content-collections/collections/products.ts
+++ packages/content-collections/collections/products.ts
@@ -1,22 +1,45 @@
 import crypto from 'crypto'
 import { defineCollection } from '@content-collections/core'
-import { image } from '../utils'
+import { compileMDX } from '@content-collections/mdx'
+import { remarkGfm } from 'fumadocs-core/mdx-plugins'
+import { getSlugAndLocale, image, seo } from '../utils'
 
 export const products = defineCollection({
 	name: 'products',
 	directory: 'content/products',
 	include: '**/*.mdx',
 	schema: (z) => ({
+		seo,
 		title: z.string(),
 		description: z.string(),
 		url: z.string().url().optional(),
 		sourceUrl: z.string().url().optional(),
-		image,
+		icon: image,
+		images: z.array(image).optional(),
 	}),
-	transform: async (item) => {
-		return {
-			id: crypto.randomUUID(),
-			...item,
+	transform: async ({ title, ...item }, context) => {
+		try {
+			const body = await context.cache(item.content, async () =>
+				compileMDX(context, item, {
+					remarkPlugins: [remarkGfm],
+					rehypePlugins: [],
+				}),
+			)
+
+			return {
+				id: crypto.randomUUID(),
+				...item,
+				title,
+				body,
+				...getSlugAndLocale(item._meta.path),
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				console.error('Failed to transform content:', error)
+				throw new Error(`Failed to transform content: ${error.message}`)
+			}
+
+			throw new Error('Failed to transform content')
 		}
 	},
 })
