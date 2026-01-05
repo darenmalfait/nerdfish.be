f00541960350ba5a89b56a3ebb1fb9c82475fd3d
Daren Malfait <56068461+darenmalfait@users.noreply.github.com>
2026-01-05 16:35:18 +0100

    feat(timesheets): day event styling

--- packages/timesheets/components/timesheets-calendar-events.tsx
+++ packages/timesheets/components/timesheets-calendar-events.tsx
@@ -1,6 +1,12 @@
 'use client'
 
 import { Badge } from '@nerdfish/react/badge'
+import {
+	TooltipProvider,
+	Tooltip,
+	TooltipTrigger,
+	TooltipContent,
+} from '@nerdfish/react/tooltip'
 import { secondsToHoursAndMinutes } from '@repo/calendar/utils'
 import { cn } from '@repo/lib/utils/class'
 import { type TimesheetsRecord } from '../schemas'
@@ -13,17 +19,43 @@ export function TimesheetsCalendarEvents({
 	isToday: boolean
 }) {
 	return (
-		<div className="flex w-full flex-col space-y-2 font-sans">
-			{data.length > 0 ? (
-				<Badge
-					className={cn(isToday && 'bg-accent text-accent-foreground')}
-					key={data[0]?.id}
-				>
-					{data[0]?.project} ({secondsToHoursAndMinutes(data[0]?.duration ?? 0)}
-					)
-				</Badge>
-			) : null}
-			{data.length > 1 ? <Badge>+{data.length - 1} more</Badge> : null}
-		</div>
+		<TooltipProvider>
+			<div className="space-y-sm flex w-full flex-col font-sans">
+				{data.length > 0 ? (
+					<Tooltip>
+						<TooltipTrigger>
+							<Badge
+								className={cn(
+									isToday &&
+										'bg-accent text-accent-foreground gap-sm flex w-full items-center justify-between',
+								)}
+								key={data[0]?.id}
+							>
+								<span className="text-foreground-inverted line-clamp-1 w-full flex-1 text-sm">
+									{data[0]?.project}
+								</span>
+								<span className="text-foreground-inverted/60 shrink-0 text-sm">
+									({secondsToHoursAndMinutes(data[0]?.duration ?? 0)})
+								</span>
+							</Badge>
+						</TooltipTrigger>
+						<TooltipContent>
+							<p>{data[0]?.project}</p>
+							<p>({secondsToHoursAndMinutes(data[0]?.duration ?? 0)})</p>
+						</TooltipContent>
+					</Tooltip>
+				) : null}
+				{data.length > 1 ? (
+					<Tooltip>
+						<TooltipTrigger>
+							<Badge>+{data.length - 1} more</Badge>
+						</TooltipTrigger>
+						<TooltipContent>
+							<p>{data.map((event) => event.project).join(', ')}</p>
+						</TooltipContent>
+					</Tooltip>
+				) : null}
+			</div>
+		</TooltipProvider>
 	)
 }
