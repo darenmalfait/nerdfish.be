d8f302990f4b467d47830182aca3eb1ac5c924e4
Daren Malfait <56068461+darenmalfait@users.noreply.github.com>
2026-01-13 17:21:21 +0100

    chore(auth): add package

---
+++ packages/auth/keys.ts
@@ -0,0 +1,32 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const keys = () =>
+	createEnv({
+		server: {
+			CLERK_SECRET_KEY: z.string().startsWith('sk_').optional(),
+			CLERK_WEBHOOK_SECRET: z.string().startsWith('whsec_').optional(),
+		},
+		client: {
+			NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z
+				.string()
+				.startsWith('pk_')
+				.optional(),
+			NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string().startsWith('/'),
+			NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string().startsWith('/'),
+			NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: z.string().startsWith('/'),
+			NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: z.string().startsWith('/'),
+		},
+		runtimeEnv: {
+			CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+			CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET,
+			NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+				process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+			NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+			NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+			NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL:
+				process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+			NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL:
+				process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+		},
+	})
---
+++ packages/auth/provider.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import { type Theme } from '@clerk/types'
+import { useTheme } from 'next-themes'
+import { type ComponentProps } from 'react'
+
+type AuthProviderProperties = ComponentProps<typeof ClerkProvider> & {
+	privacyUrl?: string
+	termsUrl?: string
+	helpUrl?: string
+}
+
+export function AuthProvider({
+	privacyUrl,
+	termsUrl,
+	helpUrl,
+	...properties
+}: AuthProviderProperties) {
+	const { resolvedTheme } = useTheme()
+	const isDark = resolvedTheme === 'dark'
+	const baseTheme = isDark ? dark : undefined
+
+	const variables: Theme['variables'] = {
+		fontFamily: 'var(--font-sans)',
+		fontFamilyButtons: 'var(--font-sans)',
+		fontWeight: {
+			bold: 'var(--font-weight-bold)',
+			normal: 'var(--font-weight-normal)',
+			medium: 'var(--font-weight-medium)',
+		},
+	}
+
+	const elements: Theme['elements'] = {
+		dividerLine: 'bg-border',
+		socialButtonsIconButton: 'bg-background-muted',
+		navbarButton: 'text-foreground',
+		organizationSwitcherTrigger__open: 'bg-background',
+		organizationPreviewMainIdentifier: 'text-foreground',
+		organizationSwitcherTriggerIcon: 'text-foreground-muted',
+		organizationPreview__organizationSwitcherTrigger: 'gap-best-friends',
+		organizationPreviewAvatarContainer: 'shrink-0',
+	}
+
+	const layout: Theme['layout'] = {
+		privacyPageUrl: privacyUrl,
+		termsPageUrl: termsUrl,
+		helpPageUrl: helpUrl,
+	}
+
+	return (
+		<ClerkProvider
+			{...properties}
+			appearance={{ layout, baseTheme, elements, variables }}
+		/>
+	)
+}
